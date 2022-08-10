af696f3ed0f9e8474c4316eff251b221e3ebffd2
Max <m.k4rp0v@yandex.ru>
2022-08-10 13:08:40 +0300

    321

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from time import asctime
 from flask import Flask
 from flask import request
-from fast_bitrix24 import Bitrix
+from fast_bitrix24 import BitrixAsync
 import asyncio
 
 webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
-b = Bitrix(webhook)
+b = BitrixAsync(webhook)
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'HEAD', 'GET'])
-async def result():
+def result():
 
-     await update_code_1c(request.form)
+    update_code_1c(request.form)
 
      return 'OK'
 
@@ -27,12 +27,11 @@ def update_code_1c(req):
     for product in products:
         product_fields = b.get_all('crm.product.get', {'id': product['PRODUCT_ID']})  # Получение полей продукта
         code_1c = product_fields['PROPERTY_139']['value']  # Получение кода 1С
-        a = b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
+        await b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
 
         with open('logs.txt', 'a') as file:
             file.write(f"DEAL_ID: {deal_id} {asctime()}")
 
-        return(a)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

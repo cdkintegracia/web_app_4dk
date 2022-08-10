fc4c5d876dcd74374f7914d4b7e1a3b5e7efbaa1
Max <m.k4rp0v@yandex.ru>
2022-08-10 13:35:46 +0300

    1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from time import asctime
-from aioflask import Flask
+from flask import Flask
 from flask import request
 from fast_bitrix24 import BitrixAsync
 import asyncio
@@ -11,14 +11,14 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'HEAD', 'GET'])
-async def result():
+def result():
 
-    await update_code_1c(request.form)
+    update_code_1c(request.form)
 
     return 'OK'
 
 
-async def update_code_1c(req):
+def update_code_1c(req):
 
     deal_id = req['data[FIELDS][ID]']
 
@@ -27,7 +27,7 @@ async def update_code_1c(req):
     for product in products:
         product_fields = b.get_all('crm.product.get', {'id': product['PRODUCT_ID']})  # Получение полей продукта
         code_1c = product_fields['PROPERTY_139']['value']  # Получение кода 1С
-        await b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
+        b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
 
         with open('logs.txt', 'a') as file:
             file.write(f"DEAL_ID: {deal_id} {asctime()}")

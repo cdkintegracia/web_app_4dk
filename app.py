d35103b0e8c1b09eea58df28e12c34a2e06b3bc8
Max <m.k4rp0v@yandex.ru>
2022-08-10 12:49:22 +0300

    123

--- app.py
+++ app.py
@@ -15,29 +15,22 @@ def result():
 
      deal_id = request.form['data[FIELDS][ID]']     # ID из POST запроса
 
-     job(deal_id)
+     asyncio.run(update_code_1c(deal_id))
 
      return 'OK'
 
-def update_code_1c(deal_id):
+async def update_code_1c(deal_id):
 
     products = b.get_all('crm.deal.productrows.get', {'id': deal_id})  # Получение информации о продукте сделки
 
     for product in products:
         product_fields = b.get_all('crm.product.get', {'id': product['PRODUCT_ID']})  # Получение полей продукта
         code_1c = product_fields['PROPERTY_139']['value']  # Получение кода 1С
-        b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
+        await b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
 
         with open('logs.txt', 'a') as file:
             file.write(f"DEAL_ID: {deal_id} {asctime()}")
 
-    return
-
-async def job(deal_id):
-
-    result = await update_code_1c(deal_id)
-
-    return 'job is done'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

766eb5764f7d81d9646556c43d1e4cd387e6a8c7
Max <m.k4rp0v@yandex.ru>
2022-08-10 15:08:03 +0300

    upd

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from time import asctime
 from flask import Flask
 from flask import request
-from fast_bitrix24 import BitrixAsync
-import asyncio
+import requests
 
 webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
-b = BitrixAsync(webhook)
 
 app = Flask(__name__)
 
@@ -18,20 +16,34 @@ def result():
     return 'OK'
 
 
-async def update_code_1c(req):
+def update_code_1c(req):
 
     deal_id = req['data[FIELDS][ID]']
 
-    products = await b.get_all('crm.deal.productrows.get', {'id': deal_id})  # Получение информации о продукте сделки
-    for product in products:
-        product_fields = b.get_all('crm.product.get', {'id': product['PRODUCT_ID']})  # Получение полей продукта
-        code_1c = product_fields['PROPERTY_139']['value']  # Получение кода 1С
-        upd = await b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
+    # Получение информации о продукте сделки
 
-        with open('logs.txt', 'a') as file:
-            file.write(f"DEAL_ID: {deal_id} {asctime()}")
+    deal_product = requests.get(
+        url=webhook + 'crm.deal.productrows.get.json?id=' + deal_id
+    )
 
-    return True
+    # ID продукта сделки
+
+    id_deal_product = str(deal_product.json()['result'][0]['PRODUCT_ID'])
+
+    # Получение полей продукта
+
+    product_fields = requests.get(url=webhook + 'crm.product.get.json?id=' + id_deal_product)
+
+    # Получение кода 1С
+
+    code_1c = product_fields.json()['result']['PROPERTY_139']['value']
+
+    # Запись кода в сделку
+
+    requests.get(url=f"{webhook}crm.deal.update?id=84621&fields[UF_CRM_1655972832]={code_1c}")
+
+    with open('logs.txt', 'a') as file:
+        file.write(f"DEAL_ID: {deal_id} {asctime()}")
 
 
 if __name__ == '__main__':

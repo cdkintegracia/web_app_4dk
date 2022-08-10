7411890c45d0551e7f1b4a111048ad5eeff9846e
Max <m.k4rp0v@yandex.ru>
2022-08-10 11:03:58 +0300

    GET

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def result():
      print(request.form)
      return 'OK'

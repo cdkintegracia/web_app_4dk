f71b19a817a33818a1cab6209fb36b00d705fbe1
Max <m.k4rp0v@yandex.ru>
2022-08-10 12:59:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def update_code_1c(deal_id):
     for product in products:
         product_fields = b.get_all('crm.product.get', {'id': product['PRODUCT_ID']})  # Получение полей продукта
         code_1c = product_fields['PROPERTY_139']['value']  # Получение кода 1С
-        await b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
+        b.call('crm.deal.update', {'ID': deal_id, 'fields': {'UF_CRM_1655972832': code_1c}})  # Запись кода в сделку
 
         with open('logs.txt', 'a') as file:
             file.write(f"DEAL_ID: {deal_id} {asctime()}")

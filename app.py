cd5aeabd85fb8ac1c7aa0ba361d60e819cb4bc38
Max <m.k4rp0v@yandex.ru>
2022-08-10 10:59:28 +0300

    1

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-import requests
-import json
 from flask import Flask
-from flask import request
-
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
-@app.route('/', methods=['POST'])
-def result():
-     print(request.form)
-     return 'OK'
-
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="141.8.192.155", port=8080)
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')

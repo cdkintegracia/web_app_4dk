445f89d2557a297a33796660979b5b35edfbdae0
Max <m.k4rp0v@yandex.ru>
2022-08-10 11:20:00 +0300

    1

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import requests
 import json
 from flask import Flask
 from flask import request
+from fast_bitrix24 import Bitrix
+
+webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
+b = Bitrix(webhook)
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET', 'HEAD'])
+@app.route('/', methods=['POST', 'HEAD'])
 def result():
-     print(request.form)
+     print(request.json)
      return 'OK'
 
 

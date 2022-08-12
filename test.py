55198ee5434b8a6dac44592c3fd9fd4af9e4406f
Max <m.k4rp0v@yandex.ru>
2022-08-12 15:36:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def result():
             print('Подходит')
             return 'ura'
         else:
-            print('Не подходит', type(url_data), len(url_data), request.data)
+            print('Не подходит', type(url_data), len(url_data), request.form)
             return 'oke'
     else:
         return 'OK'

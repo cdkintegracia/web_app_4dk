a1ef232837b8ec8f0d389afe8e3179b3c6319807
Max <m.k4rp0v@yandex.ru>
2022-08-12 14:26:48 +0300

    1

---
+++ test.py
@@ -0,0 +1,20 @@
+from time import asctime
+from flask import Flask
+from flask import request
+import requests
+
+
+webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
+
+app = Flask(__name__)
+
+logs = []
+
+
+@app.route('/', methods=['POST', 'HEAD', 'GET'])
+def result():
+    print(request.form)
+    return request.form
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')

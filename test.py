8227ad51f795c0f8561c65dc102de3f6c05508c7
Max <m.k4rp0v@yandex.ru>
2022-08-12 10:15:29 +0300

    123

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import schedule
 def ok():
     print('10101010101010101001100101010110')
-schedule.every(1).minutes.do(ok)
+schedule.every(10).minutes.do(ok)
 while True:
     schedule.run_pending()

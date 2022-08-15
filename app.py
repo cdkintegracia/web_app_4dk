5c6e544bbdfcb591c45f7832b868a52d01e1edbf
Max <m.k4rp0v@yandex.ru>
2022-08-15 17:58:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -298,9 +298,8 @@ custom_webhooks = {'create_task_service': create_task_service}
 
 @app.route('/tasks.php', methods=['POST', 'HEAD', 'GET'])
 def text():
-    data = open(request.form, 'rb')
     #php = subprocess.run(["/usr/bin/php", "/root/flask/tasks.php", request.form])
-    print(data)
+    print(request.get_json())
     return 'OK'
 
 @app.route('/', methods=['POST', 'HEAD', 'GET'])

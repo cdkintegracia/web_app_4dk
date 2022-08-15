9648419e97ddb1b1f7fd71fe45236c54b4ce63ed
Max <m.k4rp0v@yandex.ru>
2022-08-15 16:07:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -242,6 +242,8 @@ def create_task_service(dct):
             ], raw=True
                                 )
 
+        # Защита от дублирования задач
+
         updated_task = b.get_all('tasks.task.get', {'taskId': task['task']['id']})
         if len(updated_task['task']['checklist']) == 0:
             b.call('tasks.task.delete', {'taskId': task['task']['id']})

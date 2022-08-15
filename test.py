1549723892b71d890e2b5000b036cc798cff2dd4
Max <m.k4rp0v@yandex.ru>
2022-08-15 11:39:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -133,7 +133,7 @@ def create_task_service(dct):
             b.call('tasks.task.add', {
                 'fields': {
                     'TITLE': f"Сервисный выезд {employee_name} {months[dct['month']]}",
-                    'DEADLINE': f"{str({year})}-{month}-{current_month_days} 19:00:00",
+                    'DEADLINE': f"{str(year)}-{month}-{current_month_days} 19:00:00",
                     'RESPONSIBLE_ID': '311',
                     'ALLOW_CHANGE_DEADLINE': 'N',
 

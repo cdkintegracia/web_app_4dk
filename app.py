71b9917b2cca4ff4e5ac3b38ae9bbcbdc856b095
Max <m.k4rp0v@yandex.ru>
2022-08-18 16:05:12 +0300

    app

--- app.py
+++ app.py
@@ -294,13 +294,23 @@ def update_code_1c(deal_id):
 
 
 def update_company_value(deal_id):
+    """
+    :param deal_id: ID удаленной сделки
+    :return: Запуск БП "Вес сделок" с новым значением поля "Вес компании"
+    """
+
+    # Нахождение компании, к которой привязана сделка
+
     company = b.get_all(
         'crm.company.list', {
             'filter': {'UF_CRM_1660824010': deal_id}
         }
     )[0]['ID']
     company_id = f'COMPANY_{company}'
-    bp = b.call('bizproc.workflow.start', {
+
+    # Запуск БП "Вес сделок"
+
+    b.call('bizproc.workflow.start', {
         'TEMPLATE_ID': '1031',
         'DOCUMENT_ID': ['crm', 'CCrmDocumentCompany', company_id],
         'PARAMETERS': {'process': 'Удаление', 'src': deal_id, 'new_value': '1'}

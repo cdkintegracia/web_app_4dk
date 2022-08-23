0eaa3d259d21fac704e542cada0bd66872b9c508
Max <m.k4rp0v@yandex.ru>
2022-08-23 14:40:18 +0300

    123

--- UpdateCallStatistic.py
+++ UpdateCallStatistic.py
@@ -1,5 +1,8 @@
 from authentication import authentication
 from fast_bitrix24 import Bitrix
+from time import strftime
+
+
 """
 call_types:
 1 - входящий
@@ -23,3 +26,31 @@ employee_numbers = [
 def update_call_statistic(client_number, employee_number):
     if employee_number in employee_numbers:
         print('----------------------------------------------------')
+        print('ЗВОНОК В ФИЛТРЕ')
+        print('----------------------------------------------------')
+    month_string = {
+        '01': 'Январь',
+        '02': 'Февраль',
+        '03': 'Март',
+        '04': 'Апрель',
+        '05': 'Май',
+        '06': 'Июнь',
+        '07': 'Июль',
+        '08': 'Август',
+        '09': 'Сентябрь',
+        '10': 'Октябрь',
+        '11': 'Ноябрь',
+        '12': 'Декабрь'
+    }
+    current_date = f'{month_string[strftime("%m")]} {strftime("%Y")}'
+    
+    # ID контакта через номер телефона
+
+    contact = b.get_all('telephony.externalCall.searchCrmEntities', {'PHONE_NUMBER': client_number})
+    contact_id = contact[0]['CRM_ENTITY_ID']
+
+    # Компании, связанные с контактом | заполнение УС "Статистика звонков"
+
+    companies = b.get_all('crm.contact.company.items.get', {'id': contact_id})
+    for company in companies:
+        pass

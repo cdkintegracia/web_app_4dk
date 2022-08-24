6a6f94f2642226745cbfe2ec3b089c75b9607081
Max <m.k4rp0v@yandex.ru>
2022-08-24 15:13:03 +0300

    Update TaskService.py

--- TaskService.py
+++ TaskService.py
@@ -255,7 +255,7 @@ def create_task_service(dct):
                     'ALLOW_CHANGE_DEADLINE': 'N',
                     'GROUP_ID': '13',
                     'DESCRIPTION': f"{task_text}\n"
-                                   f"Ссылка на компанию: https://vc4dk.bitrix24.ru/crm/company/details/{company[0]['COMPANY_ID']}/",
+                                   f"Ссылка на компанию: https://vc4dk.bitrix24.ru/crm/company/details/{company[0]['ID']}/",
                     'TASK_CONTROL': 'Y',
                     'UF_CRM_TASK': 'D_' + deal['ID'],
                     'PARENT_ID': task['task']['id']

d98ea5f072c4a74eda333aeeb93d80e1b9198797
Max <m.k4rp0v@yandex.ru>
2022-08-24 13:31:19 +0300

    321

--- UpdateCallStatistic.py
+++ UpdateCallStatistic.py
@@ -148,7 +148,7 @@ def update_call_statistic(req):
                         'TITLE': f"Для компании {company_name} превышен лимит звонков",
                         'GROUP_ID': '13',
                         'RESPONSIBLE_ID': '311',
-                        'UF_CRM_TASK': "CO_" + str(company['COMPANY_ID'])
+                        'DESCRIPTION' : f"Ссылка на компанию: https://vc4dk.bitrix24.ru/crm/company/details/{company['COMPANY_ID']}/"
                     }
                 }
                        )

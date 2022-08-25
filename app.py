0c469acebc39f1f9ba66fc45c249d6492ab6028c
Max <m.k4rp0v@yandex.ru>
2022-08-25 10:17:52 +0300

    1

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
 from TaskService import create_task_service
 from UpdateCompanyValue import update_company_value
 from UpdateCode1C import update_code_1c
@@ -7,16 +6,6 @@ from UpdateCallStatistic import update_call_statistic
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    goal = db.Column(db.Integer)
 
 
 # Словарь функций для вызова из кастомного запроса
@@ -48,19 +37,6 @@ def custom_webhook():
     custom_webhooks[job](request.args)
     return 'OK'
 
-
-@app.route('/', methods=['HEAD', 'GET'])
-def site():
-
-    return render_template('index.html')
-
-
-@app.route('/<login>/<password>/<goal>')
-def index(login, password, goal):
-    user = User(login=login, password=password, goal=goal)
-    db.session.add(user)
-    db.session.commit()
-    return '<h1>New user!</h1>'
 
 
 if __name__ == '__main__':

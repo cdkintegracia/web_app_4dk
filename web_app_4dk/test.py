dbd156490d14e20d7f7dce56d8dbd4349f0711d8
Max <m.k4rp0v@yandex.ru>
2022-08-26 16:14:28 +0300

    1

--- web_app_4dk/test.py
+++
@@ -1,8 +0,0 @@
-from fast_bitrix24 import Bitrix
-
-
-webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
-b = Bitrix(webhook)
-
-t = b.call('tasks.task.renew', {'taskId': '50699'})
-print(t)

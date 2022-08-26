d352b94e769ec607a210b3450654c621e5f4a082
Max <m.k4rp0v@yandex.ru>
2022-08-26 17:47:37 +0300

    a

--- web_app_4dk/CheckTaskResult.py
+++ web_app_4dk/CheckTaskResult.py
@@ -3,7 +3,6 @@ from fast_bitrix24 import Bitrix
 from web_app_4dk.authentication import authentication
 
 # Считывание файла authentication.txt
-
 webhook = authentication('Bitrix')
 b = Bitrix(webhook)
 
@@ -11,10 +10,10 @@ b = Bitrix(webhook)
 
 def check_task_result(dct):
     id = dct['id']
-    task = b.get_all('tasks.task.list', {'params': {'WITH_RESULT_INFO': 'true'}, 'select': ['ID'], 'filter': {'ID': id}})[0]
-    print(task)
-    if task['taskHasResult'] == 'N':
-        b.call('tasks.task.update', {'taskId': task['id'], 'fields': {'STAGE_ID': '1117', 'STATUS': '2'}})
-    else:
-        t = b.call('tasks.task.update', {'taskId': task['id'], 'fields': {'STAGE_ID': '1121'}})
-        print(t)
+    task = b.get_all('task.commentitem.getlist', {'ID': id})
+    for comment in task:
+        if '[USER=333]' in comment['POST_MESSAGE']:
+            return
+    b.call('tasks.task.update', {'taskId': id, 'fields': {'STAGE_ID': '1121'}})
+
+
--- web_app_4dk/UpdateContactPhoto.py
+++ web_app_4dk/UpdateContactPhoto.py
@@ -4,4 +4,4 @@ webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
 b = Bitrix(webhook)
 
 
-upd = b.call('crm.contact.update', {'filter': {'ID': '11023'}, 'fields': {'PHOTO': })
+upd = b.call('crm.contact.update', {'filter': {'ID': '11023'}, 'fields': {'PHOTO': 1}})

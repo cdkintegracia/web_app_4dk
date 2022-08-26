692bf7e4bf60ebbbfd1470d3a58a4bd7c278e9bb
Max <m.k4rp0v@yandex.ru>
2022-08-26 17:27:14 +0300

    1

---
+++ web_app_4dk/UpdateContactPhoto.py
@@ -0,0 +1,7 @@
+from fast_bitrix24 import Bitrix
+
+webhook = 'https://vc4dk.bitrix24.ru/rest/311/r1oftpfibric5qym/'
+b = Bitrix(webhook)
+
+
+upd = b.call('crm.contact.update', {'filter': {'ID': '11023'}, 'fields': {'PHOTO': })

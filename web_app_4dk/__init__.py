cc6cf9cf0bcfe71a74f1bd43d9539e307d9544cf
Max <m.k4rp0v@yandex.ru>
2022-08-31 18:18:47 +0300

    321

--- web_app_4dk/__init__.py
+++ web_app_4dk/__init__.py
@@ -63,14 +63,11 @@ def read_logs():
         logs = log_file.readlines()
         for s in logs:
             info_text = s.split('request: ')[0]
-            request_text = s.split('request: ')[1].lstrip('{')
-            request_text.rstrip('}')
+            request_text = s.split('request: ')[1].lstrip('{"')
+            request_text.rstrip('}"')
             request_text = request_text.split(',')
-            temp_dct = {}
-            print(request_text)
-            for pair in request_text:
-                temp_dct.setdefault(pair[0], pair[1])
-            final_text.append([info_text, temp_dct])
+            final_text.append([info_text, request_text])
+            print(final_text[-1][1])
         return final_text[::-1]
 
 

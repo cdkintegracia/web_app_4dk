2c2b418ccf1d23c01c67b77c6ac077cfae1b754e
Max <m.k4rp0v@yandex.ru>
2022-09-05 11:17:47 +0300

    1

--- web_app_4dk/test.py
+++
@@ -1,9 +0,0 @@
-from fast_bitrix24 import Bitrix
-
-b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
-
-b.call('bizproc.workflow.start', {
-        'TEMPLATE_ID': '1095',
-        'DOCUMENT_ID':
-            ['lists', 'BizprocDocument', '128131'],
-        'PARAMETERS': {'user': '311', 'message': f"Сверка ИТС завершена"}})

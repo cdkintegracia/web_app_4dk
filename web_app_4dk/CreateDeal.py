3c10a5583b1d7e730dd6ec8322429474a7cbfe57
Max <m.k4rp0v@yandex.ru>
2022-09-12 17:54:43 +0300

    Update CreateDeal.py

--- web_app_4dk/CreateDeal.py
+++ web_app_4dk/CreateDeal.py
@@ -3,6 +3,7 @@ from fast_bitrix24 import Bitrix
 b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
 
 def create_deal():
+    pass
     ignore_fields = [
         'ID',
         'TITLE',

0c487c7890c6535b723e0af5265b37c4d98b475a
korpovmoxem <m.k4rp0v@yandex.ru>
2022-09-16 11:46:27 +0300

    Update Connect1C.py

--- web_app_4dk/Connect1C.py
+++ web_app_4dk/Connect1C.py
@@ -129,8 +129,8 @@ def connect_1c(req: dict):
     with open('/root/web_app_4dk/web_app_4dk/static/logs/connect.json', 'r') as file:
         data = json.load(file)
         data.append(req)
-    with open('/root/web_app_4dk/web_app_4dk/static/logs/connect.json', 'w') as file:
-        son.dump(data, file, indent=4, ensure_ascii=False)
+        with open('/root/web_app_4dk/web_app_4dk/static/logs/connect.json', 'w') as file:
+            son.dump(data, file, indent=4, ensure_ascii=False)
 
     # Начало обращения. Создание задачи
     if req['message_type'] in [80, 81]:

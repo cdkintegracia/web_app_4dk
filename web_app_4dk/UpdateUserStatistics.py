e869327868dd6dd3faf238819736ca6ef14e2cc7
Max <m.k4rp0v@yandex.ru>
2022-09-22 16:37:52 +0300

    Update UpdateUserStatistics.py

--- web_app_4dk/UpdateUserStatistics.py
+++ web_app_4dk/UpdateUserStatistics.py
@@ -56,6 +56,7 @@ def add_call(req: dict):
 def add_mail(req: dict):
     activity_type = b.get_all('crm.activity.list', {'filter': {'ID': req['data[FIELDS][ID]']}})[0]
     print(activity_type)
+    return
     if activity_type['PROVIDER_TYPE_ID'] == 'EMAIL':
         data_to_write = [req['event'],
                          get_user_name(req['AUTHOR_ID']),

39b4112678f1483ba839e419d15d1b08e7917d39
Max <m.k4rp0v@yandex.ru>
2022-09-23 10:38:08 +0300

    Update UpdateUserStatistics.py

--- web_app_4dk/UpdateUserStatistics.py
+++ web_app_4dk/UpdateUserStatistics.py
@@ -59,6 +59,7 @@ def add_call(req: dict):
 
 def add_mail(req: dict):
     activity_type = requests.post(f"{authentication('Bitrix')}'crm.activity.get'?id={req['data[FIELDS][ID]']}").json()
+    print(activity_type)
     if activity_type['PROVIDER_TYPE_ID'] == 'EMAIL':
         return
         data_to_write = [req['event'],

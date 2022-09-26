a53b87d3ab79e6c041390ab1f81150ffae9e6bc2
korpovmoxem <m.k4rp0v@yandex.ru>
2022-09-26 12:38:04 +0300

    1

--- web_app_4dk/Connect1C.py
+++ web_app_4dk/Connect1C.py
@@ -139,7 +139,7 @@ def connect_1c(req: dict):
         return
     # Запись события в логи
     log = f"{req['message_type']}|{req['author_id']}|{req['treatment_id']}|{req['line_id']}|{get_event_info(req)}\n"
-    with open('/root/web_app_4dk/web_app_4dk/static/logs/connect.json', 'a') as file:
+    with open('/root/web_app_4dk/web_app_4dk/static/logs/connect.txt', 'a') as file:
         file.write(log)
 
     """

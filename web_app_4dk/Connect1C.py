ee1296d85776d27b98de8af09bda1dc524d9b828
korpovmoxem <m.k4rp0v@yandex.ru>
2022-09-27 12:31:47 +0300

    Update Connect1C.py

--- web_app_4dk/Connect1C.py
+++ web_app_4dk/Connect1C.py
@@ -217,7 +217,7 @@ def connect_1c(req: dict):
         'filter': {
             'UF_AUTO_499889542776': req['treatment_id']}})
     if is_task_created:
-        connect_user_name = get_name(req['author_id'])
+        connect_user_name = get_name(req['author_id'])[0]
         connect_user_id = get_employee_id(connect_user_name)
         if connect_user_id == '0':
             return

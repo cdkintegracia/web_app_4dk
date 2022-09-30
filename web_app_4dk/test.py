8a3e158bf9fbdbb3f5f00d341c0a5d7ad1881e0e
Max <m.k4rp0v@yandex.ru>
2022-10-01 01:38:36 +0300

    cv

--- web_app_4dk/test.py
+++ web_app_4dk/test.py
@@ -3,6 +3,6 @@ import requests
 
 b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
 
-contact = b.get_all('crm.contact.get', {'ID': '11293'})
-photo_id = contact['PHOTO']['id']
-b.call('crm.contact.update', {'ID': '11293', 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
+contact_photo = b.get_all('crm.contact.list', {'select': ['PHOTO'], 'filter': {'ID': '11293'}})[0]
+
+print(contact_photo)

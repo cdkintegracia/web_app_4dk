32efdfc4a20dc4c189fc8fbf3fc56cb2568f91c4
Max <m.k4rp0v@yandex.ru>
2022-10-01 01:28:01 +0300

    x

--- web_app_4dk/UpdateContactPhoto.py
+++ web_app_4dk/UpdateContactPhoto.py
@@ -9,11 +9,11 @@ b = Bitrix(authentication('Bitrix'))
 
 def update_contact_photo(req: dict):
     contact_id = req['data[FIELDS][ID]']
-    contact_photo = b.get_all('crm.contact.list', {'selecr': ['PHOTO'], 'filter': {'ID': contact_id}})[0]
+    contact_photo = b.get_all('crm.contact.list', {'select': ['PHOTO'], 'filter': {'ID': contact_id}})[0]
     companies = b.get_all('crm.contact.company.items.get', {'ID': contact_id})
     if companies and 'PHOTO' in contact_photo:
-        return
-        b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': None}})
+        photo_id = contact_photo['PHOTO']['id']
+        b.call('crm.contact.update', {'ID': '11293', 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
     elif not companies and 'PHOTO' not in contact_photo:
         with open('/root/web_app_4dk/web_app_4dk/red_square.png', 'rb') as file:
             photo = file.read()
--- web_app_4dk/test.py
+++ web_app_4dk/test.py
@@ -1,5 +1,8 @@
 from fast_bitrix24 import Bitrix
+import requests
 
+b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
 
-
-b.call('crm.contact.update', {'ID': '11293', 'fields': {'PROPERTY_360_DEL': 'None'}})
+contact = b.get_all('crm.contact.get', {'ID': '11293'})
+photo_id = contact['PHOTO']['id']
+b.call('crm.contact.update', {'ID': '11293', 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})

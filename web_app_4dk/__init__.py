d568abd3eea065b21f5630e056ca92b29664261d
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-04 17:48:59 +0300

    1

--- web_app_4dk/__init__.py
+++ web_app_4dk/__init__.py
@@ -67,7 +67,7 @@ def main_page():
         if not new_call_statistic_file:
             flash('Необходимо загрузить файл')
         else:
-            new_call_statistic_file.save('./')
+            new_call_statistic_file.save('new_call_statistic.xlsx')
     return render_template('main_page.html', web_app_logs=read_logs())
 
 # Обработчик вебхуков 1С-Коннект

2728acb43ec86d975c25d28e170db049b44ddf3a
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-04 10:43:14 +0300

    1

--- web_app_4dk/Prolongation_ITS.py
+++ web_app_4dk/Prolongation_ITS.py
@@ -16,7 +16,39 @@ def prolongation_its(req):
     year = strftime('%Y')
     date_filter_start = f"{year}-{month}-01"
     date_filter_end = f"{year}-{month}-{monthrange(int(year), int(month))[1]}"
-    deals = b.get_all('crm.deal.list', {'filter': {'ASSIGNED_BY_ID': users_id, '<=CLOSEDATE': date_filter_end, '>=CLOSEDATE': date_filter_start}})
+    deals_main = b.get_all('crm.deal.list',
+                               {'select': [
+                                   'COMPANY_ID',
+                                   'TITLE',
+                                   'ID',
+                                   'ASSIGNED_BY_ID'
+                               ], 'filter': {
+                                   'ASSIGNED_BY_ID': users_id,
+                                   '<=CLOSEDATE': date_filter_end,
+                                   '>=CLOSEDATE': date_filter_start
+                               }
+                               }
+                               )
+    deals_sub = b.get_all('crm.deal.list',
+                               {'select': [
+                                   'COMPANY_ID',
+                                   'TITLE',
+                                   'ID',
+                                   'ASSIGNED_BY_ID'
+                               ], 'filter': {
+                                   'ASSIGNED_BY_ID': users_id,
+                                   '<=UF_CRM_1638958630625': date_filter_end,
+                                   '>=UF_CRM_1638958630625': date_filter_start
+                               }
+                               }
+                               )
+    deals = []
+    for deal in deals_main:
+        deals.append(deal)
+    for deal in deals_sub:
+        if deal not in deals:
+            deals.append(deal)
+
     for deal in deals:
         company_name = b.get_all('crm.company.list', {'filter': {'ID': deal['COMPANY_ID']}})[0]['TITLE']
         b.call('tasks.task.add', {

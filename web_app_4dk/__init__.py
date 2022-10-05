8e08310899cc3cd9d7b3b673386ccdd076799f39
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-05 13:56:22 +0300

    123

--- web_app_4dk/__init__.py
+++ web_app_4dk/__init__.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.secret_key = 'some very secret phrase for web_app_4dk'
 login_manager = LoginManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 import web_app_4dk.routes, web_app_4dk.create_db

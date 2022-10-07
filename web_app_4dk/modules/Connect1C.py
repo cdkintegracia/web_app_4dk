07c59f8f9b8da129f4f3016891b580cc2aabd87a
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-07 15:27:24 +0300

    Update Connect1C.py

--- web_app_4dk/modules/Connect1C.py
+++ web_app_4dk/modules/Connect1C.py
@@ -177,7 +177,8 @@ def connect_1c(req: dict):
         else:
             company_id = author_info[1]
 
-        support_id = get_employee_id(author_info[0])
+        support_info = get_name(req['author_id'], req['treatment_id'])
+        support_id = get_employee_id(support_info[0])
         print(support_id)
         print(author_info[0])
         responsible_id = '173'

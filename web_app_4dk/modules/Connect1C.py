4c71c35d2ec2423121e1360fd21ea8cb81b83ba9
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-07 15:42:03 +0300

    Update Connect1C.py

--- web_app_4dk/modules/Connect1C.py
+++ web_app_4dk/modules/Connect1C.py
@@ -183,7 +183,7 @@ def connect_1c(req: dict):
         print(support_id)
         print(support_info[0])
         responsible_id = '173'
-        if support_id in ['129', '127']:
+        if support_id in ['129', '127', '1']:
             responsible_id = support_id
 
         b.call('tasks.task.add', {'fields': {

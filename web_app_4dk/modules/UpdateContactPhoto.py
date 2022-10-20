55c048f280ad96ec87a57663dd82fcca3f2c028b
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-20 17:22:38 +0300

    1

--- web_app_4dk/modules/UpdateContactPhoto.py
+++ web_app_4dk/modules/UpdateContactPhoto.py
@@ -18,7 +18,7 @@ def update_contact_photo(req: dict):
         #b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
         requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
     elif not companies and 'PHOTO' not in contact or contact['PHOTO'] is None:
-        with open('/root/web_app_4dk/web_app_4dk/red_square.png', 'rb') as file:
+        with open('/root/web_app_4dk/web_app_4dk/images.png', 'rb') as file:
             photo = file.read()
         new_photo = base64.b64encode(photo)
         data = {'ID': contact_id, 'fields': {'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}}}

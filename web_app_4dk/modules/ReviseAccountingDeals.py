5ff8ab9aa3c178f3a15720664e8bc456e31f5195
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-20 17:35:46 +0300

    Update ReviseAccountingDeals.py

--- web_app_4dk/modules/ReviseAccountingDeals.py
+++ web_app_4dk/modules/ReviseAccountingDeals.py
@@ -105,7 +105,14 @@ def revise_accounting_deals(filename):
         'data': {'NAME': report_name},
         'fileContent': report_file_base64
     })
-    b.call('tasks.task.add', {'fields': {'TITLE': 'Сверка отчетности', 'RESPONSIBLE_ID': '19', 'GROUP_ID': '13', 'DESCRIPTION': upload_report["DETAIL_URL"]}})
+    b.call('tasks.task.add', {
+        'fields': {
+            'TITLE': 'Сверка отчетности',
+            'RESPONSIBLE_ID': '19',
+            'GROUP_ID': '13',
+            'DESCRIPTION': upload_report["DETAIL_URL"],
+            'CREATED_BY': '173'
+        }})
     os.remove(f'Сверка_отчетности_{create_time}.xlsx')
 
 

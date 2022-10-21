3d047886ba999402cbb4350abe56de0f3ec8ef34
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-21 16:40:14 +0300

    Update UpdateContactPhoto.py

--- web_app_4dk/modules/UpdateContactPhoto.py
+++ web_app_4dk/modules/UpdateContactPhoto.py
@@ -12,11 +12,13 @@ def update_contact_photo(req: dict):
     contact_id = req['data[FIELDS][ID]']
     contact = requests.get(url=f'{authentication("Bitrix")}crm.contact.list?select[]=PHOTO&filter[ID]={contact_id}').json()['result'][0]
     companies = requests.get(url=f'{authentication("Bitrix")}crm.contact.company.items.get?ID={contact_id}').json()['result']
-    if companies and contact['PHOTO'] is not None:
-        photo_id = contact['PHOTO']['id']
-        data = {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}}
-        #b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
-        requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
+
+    if 'PHOTO' in contact:
+        if companies and contact['PHOTO'] is not None:
+            photo_id = contact['PHOTO']['id']
+            data = {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}}
+            #b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
+            requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
     elif not companies and ('PHOTO' not in contact or contact['PHOTO'] is None):
         with open('/root/web_app_4dk/web_app_4dk/images.png', 'rb') as file:
             photo = file.read()

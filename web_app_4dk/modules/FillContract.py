cded986f14523a2b243b542930af668a7824b343
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-25 14:39:55 +0300

    Update FillContract.py

--- web_app_4dk/modules/FillContract.py
+++ web_app_4dk/modules/FillContract.py
@@ -1,10 +1,16 @@
 from petrovich.main import Petrovich
 from petrovich.enums import Case
 
+from web_app_4dk.modules.authentication import authentication
+
 
 def fill_contract(req):
+    b = authentication('Bitrix')
     p = Petrovich()
     cased_fn = p.firstname(req['first_name'], Case.GENITIVE)
     cased_ln = p.lastname(req['last_name'], Case.GENITIVE)
     cased_sn = p.middlename(req['second_name'], Case.GENITIVE)
+    b.call('im.notify.system.add', {
+        'USER_ID': '311',
+        'MESSAGE': f'{cased_ln}, {cased_fn} {cased_sn}'})
     print(cased_ln, cased_fn, cased_sn)

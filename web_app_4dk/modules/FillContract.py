7cc7d1729cdd2205b84b6f871686916c28d1b1a7
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-25 15:07:26 +0300

    Update FillContract.py

--- web_app_4dk/modules/FillContract.py
+++ web_app_4dk/modules/FillContract.py
@@ -16,6 +16,11 @@ def fill_contract(req):
     cased_fn = p.firstname(req['first_name'], Case.GENITIVE, genders[req['gender']])
     cased_ln = p.lastname(req['last_name'], Case.GENITIVE, genders[req['gender']])
     cased_sn = p.middlename(req['second_name'], Case.GENITIVE, genders[req['gender']])
-    b.call('im.notify.system.add', {
-        'USER_ID': '311',
-        'MESSAGE': f'{cased_ln} {cased_fn} {cased_sn}'})
+    b.call('bizproc.workflow.start', {
+        'TEMPLATE_ID': '1203',
+        'DOCUMENT_ID': ['crm', 'CCrmDocumentDeal', 'DEAL_' + req['deal_id']],
+        'PARAMETERS': {
+            'fio': f"{req['last_name']} {req['first_name']} {req['second_name']}",
+            'cased_fio': f"{cased_ln} {cased_fn} {cased_sn}",
+            'type': req['type']
+        }})

0b922a2a04fc7f1a06181fa6b8b84734bf5d7aa8
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-26 13:24:42 +0300

    1

--- web_app_4dk/modules/FillContract.py
+++ web_app_4dk/modules/FillContract.py
@@ -1,3 +1,4 @@
+import pymorphy2
 from petrovich.main import Petrovich
 from petrovich.enums import Case, Gender
 from fast_bitrix24 import Bitrix
@@ -8,6 +9,7 @@ from web_app_4dk.modules.authentication import authentication
 def fill_contract(req):
     b = Bitrix(authentication('Bitrix'))
     p = Petrovich()
+    morph = pymorphy2.MorphAnalyzer(lang='ru')
     genders = {
         'Мужской': Gender.MALE,
         'Женский': Gender.FEMALE,
@@ -16,6 +18,12 @@ def fill_contract(req):
     cased_fn = p.firstname(req['first_name'], Case.GENITIVE, genders[req['gender']])
     cased_ln = p.lastname(req['last_name'], Case.GENITIVE, genders[req['gender']])
     cased_sn = p.middlename(req['second_name'], Case.GENITIVE, genders[req['gender']])
+    job_post = []
+    for word in req['job_post'].split():
+        parse_word = morph.parse(word)[0]
+        cased_word = parse_word.inflect({'gent'})
+        job_post.append(cased_word[0])
+    cased_job_post = ' '.join(job_post)
     b.call('bizproc.workflow.start', {
         'TEMPLATE_ID': '1203',
         'DOCUMENT_ID': ['crm', 'CCrmDocumentDeal', 'DEAL_' + req['deal_id']],
@@ -23,5 +31,7 @@ def fill_contract(req):
             'fio': f"{req['last_name'].upper()} {req['first_name'][0].upper()}. {req['second_name'][0].upper()}.",
             'cased_fio': f"{cased_ln.upper()} {cased_fn.upper()} {cased_sn.upper()}",
             'type': req['type'],
-            'based': req['based']
+            'based': req['based'],
+            'job_post': req['job_post'].lower(),
+            'cased_job_post': cased_job_post.capitalize(),
         }})

e844ba58a58d010768b3b4dfa5faca686d0bd928
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-28 12:51:39 +0300

    1

--- web_app_4dk/modules/123.py
+++ web_app_4dk/modules/123.py
@@ -1,5 +1,6 @@
 from fast_bitrix24 import Bitrix
 from datetime import datetime, timedelta
+from time import strptime
 import dateutil.parser
 
 b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
@@ -35,24 +36,24 @@ month_codes = {
 }
 
 req = {
-    'id': '2391',
+    'id': '4397',
     'month': 'Октябрь',
     'year': '2022'
 }
 def create_company_call_report(req):
-
     # Формирование заголовков отчета
     report_created_time = datetime.now()
     company_id = req['id']
     total_duration = timedelta()
     contacts = b.get_all('crm.company.contact.items.get', {'id': company_id})
+    deals = b.get_all('crm.deal.list', {'select': ['ID'], 'filter': {'COMPANY_ID': req['id']}})
     company_info = b.get_all('crm.company.get', {'ID': company_id})
     company_name = company_info['TITLE']
-    report_data = [
+    titles = [
         # 1 строка
         [
             company_name,
-            f'{req["month"]} {req["year"]}',   # Месяц и год
+            f'{req["month"]} {req["year"]}',  # Месяц и год
             f'Дата формирования {report_created_time.strftime("%d.%m.%Y %H:%M")}',
         ],
         # 2 строка
@@ -64,8 +65,9 @@ def create_company_call_report(req):
             'Кто звонил',
         ]
     ]
-
+    report_data = []
     # Формирование отчета
+
     for contact in contacts:
         contact_info = b.get_all('crm.contact.get', {'id': contact['CONTACT_ID']})
         contact_name = f"{contact_info['LAST_NAME']} {contact_info['NAME']} {contact_info['SECOND_NAME']}"
@@ -100,10 +102,44 @@ def create_company_call_report(req):
                     ])
                     duration = call_end_time - call_start_time
                     total_duration += duration
+
+    for deal in deals:
+        not_sorted_activities = b.get_all('crm.activity.list', {
+            'filter': {
+                'OWNER_TYPE_ID': '2',
+                'OWNER_ID': deal['ID'],
+                'PROVIDER_TYPE_ID': 'CALL'
+            }})
+        activities = sorted(not_sorted_activities, key=lambda x: x['ID'])
+        for activity in activities:
+            if 'Исходящий' in activity['SUBJECT']:
+                call_start_time = dateutil.parser.isoparse(activity['START_TIME'])
+                if str(call_start_time.month) == month_codes[req['month']] and str(call_start_time.year) == req['year']:
+                    if activity['AUTHOR_ID'] == '109':  # Ридкобород
+                        continue
+                    author = b.get_all('user.get', {'ID': activity['AUTHOR_ID']})[0]
+                    if 231 not in author['UF_DEPARTMENT']:
+                        continue
+                    author_name = f"{author['NAME']} {author['LAST_NAME']}"
+                    phone_number = activity['SUBJECT'].split(' на ')[1]
+                    call_end_time = dateutil.parser.isoparse(activity['END_TIME'])
+                    call_start_time_formatted = call_start_time.strftime('%d.%m.%Y %H:%M:%S')
+                    duration = call_end_time - call_start_time
+                    report_data.append([
+                        call_start_time_formatted,
+                        contact_name,
+                        phone_number,
+                        duration,
+                        author_name,
+                    ])
+                    duration = call_end_time - call_start_time
+                    total_duration += duration
+    report_data = sorted(report_data, key=lambda x: strptime(x[0], "%d.%m.%Y %H:%M:%S"))
     report_data.append(['Итого', '', '', total_duration])
-    for i in report_data:
-        print(i)
+    report_data = titles + report_data
+
 
 create_company_call_report(req)
+
 
 
--- web_app_4dk/modules/CreateCompanyCallReport.py
+++ web_app_4dk/modules/CreateCompanyCallReport.py
@@ -2,6 +2,7 @@ import os
 from datetime import timedelta
 from datetime import datetime
 import base64
+from time import strptime
 
 import openpyxl
 from openpyxl.utils import get_column_letter
@@ -51,13 +52,14 @@ def create_company_call_report(req):
     company_id = req['id']
     total_duration = timedelta()
     contacts = b.get_all('crm.company.contact.items.get', {'id': company_id})
+    deals = b.get_all('crm.deal.list', {'select': ['ID'], 'filter': {'COMPANY_ID': req['id']}})
     company_info = b.get_all('crm.company.get', {'ID': company_id})
     company_name = company_info['TITLE']
-    report_data = [
+    titles = [
         # 1 строка
         [
             company_name,
-            f'{req["month"]} {req["year"]}',   # Месяц и год
+            f'{req["month"]} {req["year"]}',  # Месяц и год
             f'Дата формирования {report_created_time.strftime("%d.%m.%Y %H:%M")}',
         ],
         # 2 строка
@@ -69,6 +71,7 @@ def create_company_call_report(req):
             'Кто звонил',
         ]
     ]
+    report_data = []
 
     # Формирование отчета
     for contact in contacts:
@@ -105,7 +108,41 @@ def create_company_call_report(req):
                     ])
                     duration = call_end_time - call_start_time
                     total_duration += duration
+
+    for deal in deals:
+        not_sorted_activities = b.get_all('crm.activity.list', {
+            'filter': {
+                'OWNER_TYPE_ID': '2',
+                'OWNER_ID': deal['ID'],
+                'PROVIDER_TYPE_ID': 'CALL'
+            }})
+        activities = sorted(not_sorted_activities, key=lambda x: x['ID'])
+        for activity in activities:
+            if 'Исходящий' in activity['SUBJECT']:
+                call_start_time = dateutil.parser.isoparse(activity['START_TIME'])
+                if str(call_start_time.month) == month_codes[req['month']] and str(call_start_time.year) == req['year']:
+                    if activity['AUTHOR_ID'] == '109':  # Ридкобород
+                        continue
+                    author = b.get_all('user.get', {'ID': activity['AUTHOR_ID']})[0]
+                    if 231 not in author['UF_DEPARTMENT']:
+                        continue
+                    author_name = f"{author['NAME']} {author['LAST_NAME']}"
+                    phone_number = activity['SUBJECT'].split(' на ')[1]
+                    call_end_time = dateutil.parser.isoparse(activity['END_TIME'])
+                    call_start_time_formatted = call_start_time.strftime('%d.%m.%Y %H:%M:%S')
+                    duration = call_end_time - call_start_time
+                    report_data.append([
+                        call_start_time_formatted,
+                        contact_name,
+                        phone_number,
+                        duration,
+                        author_name,
+                    ])
+                    duration = call_end_time - call_start_time
+                    total_duration += duration
+    report_data = sorted(report_data, key=lambda x: strptime(x[0], "%d.%m.%Y %H:%M:%S"))
     report_data.append(['Итого', '', '', total_duration])
+    report_data = titles + report_data
 
     # Создание xlsx файла отчета
     report_name_time = report_created_time.strftime('%d-%m-%Y %H %M %S %f')

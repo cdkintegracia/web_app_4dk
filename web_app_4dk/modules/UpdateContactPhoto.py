af23f9c528dd8079830d76eb31eb600db74bf7bd
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-31 12:11:45 +0300

    1

--- web_app_4dk/modules/UpdateContactPhoto.py
+++ web_app_4dk/modules/UpdateContactPhoto.py
@@ -9,25 +9,30 @@ b = Bitrix(authentication('Bitrix'))
 
 
 def update_contact_photo(req: dict):
-    return
     contact_id = req['data[FIELDS][ID]']
-    contact = requests.get(url=f'{authentication("Bitrix")}crm.contact.list?select[]=PHOTO&filter[ID]={contact_id}').json()['result'][0]
-    companies = requests.get(url=f'{authentication("Bitrix")}crm.contact.company.items.get?ID={contact_id}').json()['result']
+    data = {
+        'select': ['PHOTO'],
+        'filter': {
+            'ID': contact_id
+        }
+    }
+    contact = requests.post(url=f'{authentication("Bitrix")}crm.contact.list', json=data).json()['result'][0]
+    data = {
+        'ID': contact_id
+    }
+    companies = requests.post(url=f'{authentication("Bitrix")}crm.contact.company.items.get', json=data).json()['result']
 
     if 'PHOTO' in contact:
         if companies and contact['PHOTO'] is not None:
             photo_id = contact['PHOTO']['id']
             data = {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}}
-            #b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': {'id': photo_id, 'remove': 'Y'}}})
             requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
-    elif not companies:
-        if 'PHOTO' not in contact or contact['PHOTO'] is None:
-            with open('/root/web_app_4dk/web_app_4dk/contact_photo.jpg', 'rb') as file:
-                photo = file.read()
-            new_photo = base64.b64encode(photo)
-            data = {'ID': contact_id, 'fields': {'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}}}
-            #bx24.callMethod('crm.contact.update', id=contact_id, fields={'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}})
-            requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
-            #b.call('crm.contact.update', {'ID': contact_id, 'fields': {'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}}})
     else:
-        return
+        if not companies:
+            if 'PHOTO' not in contact or contact['PHOTO'] in [None, 'None']:
+                with open('/root/web_app_4dk/web_app_4dk/contact_photo.jpg', 'rb') as file:
+                    photo = file.read()
+                new_photo = base64.b64encode(photo)
+                data = {'ID': contact_id, 'fields': {'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}}}
+                requests.post(url=f"{authentication('Bitrix')}crm.contact.update", json=data)
+

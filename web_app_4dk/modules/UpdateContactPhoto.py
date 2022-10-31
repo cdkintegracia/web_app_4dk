5a6937dbe33f5aa17aff0fbc292c7949758cc6fe
korpovmoxem <m.k4rp0v@yandex.ru>
2022-10-31 12:12:26 +0300

    Update UpdateContactPhoto.py

--- web_app_4dk/modules/UpdateContactPhoto.py
+++ web_app_4dk/modules/UpdateContactPhoto.py
@@ -30,7 +30,7 @@ def update_contact_photo(req: dict):
     else:
         if not companies:
             if 'PHOTO' not in contact or contact['PHOTO'] in [None, 'None']:
-                with open('/root/web_app_4dk/web_app_4dk/contact_photo.jpg', 'rb') as file:
+                with open('/root/web_app_4dk/web_app_4dk/red_square.png', 'rb') as file:
                     photo = file.read()
                 new_photo = base64.b64encode(photo)
                 data = {'ID': contact_id, 'fields': {'PHOTO': {'fileData': ['red_square.png', str(new_photo)[2:]]}}}

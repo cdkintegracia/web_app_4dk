fd0f1b96d6a47c2abe4828753cce468a154d1e63
korpovmoxem <m.k4rp0v@yandex.ru>
2022-11-02 12:23:32 +0300

    1

--- web_app_4dk/modules/fsfafsfafa.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-webhook = 'https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/'
-data = {'taskId': '77911'}
-r = requests.post(url=f"{webhook}tasks.task.get", json=data).json()['result']
-print(r)

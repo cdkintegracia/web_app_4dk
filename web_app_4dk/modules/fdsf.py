e86a67baf6bb5d4a79f22d880b58f55e04c2e704
korpovmoxem <m.k4rp0v@yandex.ru>
2022-11-17 14:23:47 +0300

    Delete fdsf.py

--- web_app_4dk/modules/fdsf.py
+++
@@ -1,9 +0,0 @@
-from fast_bitrix24 import Bitrix
-
-b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
-
-el = b.get_all('lists.element.get', {
-            'IBLOCK_TYPE_ID': 'lists',
-            'IBLOCK_ID': '175',
-            'ELEMENT_ID': '227921'})[0]
-print(el['PROPERTY_1377'])

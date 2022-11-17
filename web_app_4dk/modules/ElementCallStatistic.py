b56be45b49135773e515e37369ef14d356b28122
korpovmoxem <m.k4rp0v@yandex.ru>
2022-11-17 14:23:43 +0300

    1

--- web_app_4dk/modules/ElementCallStatistic.py
+++ web_app_4dk/modules/ElementCallStatistic.py
@@ -109,76 +109,73 @@ def sort_types(company_id):
                'UC_XJFZN4',                         # Кабинет садовода
                'UC_74DPBQ',                         # БИТРИКС24
                ]
-    type_names = {
-        'SALE': 'ИТС Земля',
-        'COMPLEX': 'СааС',
-        'UC_APUWEW': 'ИТС Земля + СааС',
-        'UC_1UPOTU': 'ИТС Бесплатный',
-        'UC_O99QUW': 'Отчетность',
-        'UC_OV4T7K': 'Отчетность (в рамках ИТС)',
-        'UC_2B0CK2': '1Спарк в договоре',
-        'UC_86JXH1': '1Спарк 3000',
-        'UC_WUGAZ7': '1СпаркПЛЮС 22500',
-        'UC_A7G0AM': '1С Контрагент',
-        'GOODS': 'ГРМ',
-        'UC_GZFC63': 'РПД',
-        'UC_QQPYF0': 'Лицензия',
-        'UC_8Z4N1O': '1С-Подпись',
-        'UC_FOKY52': '1С-Подпись 1000',
-        'UC_D1DN7U': '1С Кабинет сотрудника',
-        'UC_34QFP9': 'Уникс',
-        'UC_J426ZW': '1С Садовод',
-        'UC_H8S037': 'ЭДО',
-        'UC_8LW09Y': 'МДЛП',
-        'UC_3SKJ5M': '1С Касса',
-        'UC_4B5UQD': 'ЭТП',
-        'UC_H7HOD0': 'Коннект',
-        'UC_USDKKM': 'Медицина',
-        'SERVICE': 'Сервисное обслуживание',
-        'SERVICES': 'Услуги',
-        'UC_XJFZN4': 'Кабинет садовода',
-        'UC_BZYY0D': 'ИТС Отраслевой',
-        'UC_66Z1ZF': 'ОФД',
-        'UC_40Q6MC': 'Старт ЭДО',
-        'UC_74DPBQ': 'Битрикс24',
-        'UC_IV3HX1': 'Тестовый',
-        'UC_HT9G9H': 'ПРОФ Земля',
-        'UC_XIYCTV': 'ПРОФ Земля+Помощник',
-        'UC_5T4MAW': 'ПРОФ Земля+Облако+Помощник',
-        'UC_N113M9': 'ПРОФ Земля+Облако',
-        'UC_ZKPT1B': 'ПРОФ Облако',
-        'UC_2SJOEJ': 'ПРОФ Облако+Помощник',
-        'UC_AVBW73': 'Базовый Земля',
-        'UC_GPT391': 'Базовый Облако',
-        'UC_92H9MN': 'Индивидуальный',
-        'UC_7V8HWF': 'Индивидуальный+Облако',
-        'UC_IUJR81': 'Допы Облако',
-        'UC_2R01AE': 'Услуги (без нашего ИТС)',
-        'UC_81T8ZR': 'АОВ',
-        'UC_SV60SP': 'АОВ+Облако',
-        'UC_D7TC4I': 'ГРМ Спец',
-        'UC_K9QJDV': 'ГРМ Бизнес',
-        'UC_DBLSP5': 'Садовод+Помощник',
-        'UC_GP5FR3': 'ДУО',
-        'UC_YIAJC8': 'Лицензия с купоном ИТС',
-        '1': 'Не указан',
+    type_element_codes = {
+        'SALE': '2251',
+        'COMPLEX': '2253',
+        'UC_APUWEW': '2255',
+        'UC_1UPOTU': '2257',
+        'UC_O99QUW': '2259',
+        'UC_OV4T7K': '2261',
+        'UC_2B0CK2': '2263',
+        'UC_86JXH1': '2265',
+        'UC_WUGAZ7': '2267',
+        'UC_A7G0AM': '2269',
+        'GOODS': '2271',
+        'UC_GZFC63': '2273',
+        'UC_QQPYF0': '2275',
+        'UC_8Z4N1O': '2277',
+        'UC_FOKY52': '2279',
+        'UC_D1DN7U': '2281',
+        'UC_34QFP9': '2283',
+        'UC_J426ZW': '2285',
+        'UC_H8S037': '2287',
+        'UC_8LW09Y': '2289',
+        'UC_3SKJ5M': '2291',
+        'UC_4B5UQD': '2293',
+        'UC_H7HOD0': '2295',
+        'UC_USDKKM': '2297',
+        'SERVICE': '2299',
+        'SERVICES': '2301',
+        'UC_XJFZN4': '2303',
+        'UC_BZYY0D': '2305',
+        'UC_66Z1ZF': '2307',
+        'UC_40Q6MC': '2309',
+        'UC_74DPBQ': '2311',
+        'UC_IV3HX1': '2313',
+        'UC_HT9G9H': '2315',
+        'UC_XIYCTV': '2317',
+        'UC_5T4MAW': '2319',
+        'UC_N113M9': '2321',
+        'UC_ZKPT1B': '2323',
+        'UC_2SJOEJ': '2325',
+        'UC_AVBW73': '2327',
+        'UC_GPT391': '2329',
+        'UC_92H9MN': '2331',
+        'UC_7V8HWF': '2333',
+        'UC_IUJR81': '2335',
+        'UC_2R01AE': '2337',
+        'UC_81T8ZR': '2339',
+        'UC_SV60SP': '2341',
+        'UC_D7TC4I': '2343',
+        'UC_K9QJDV': '2345',
+        'UC_DBLSP5': '2347',
+        'UC_GP5FR3': '2349',
+        'UC_YIAJC8': '2351',
+        '1': '2353',
     }
     for type in level_1:
         if type in types:
-            return type_names[type]
+            return type_element_codes[type]
     for type in level_2:
         if type in types:
-            return type_names[type]
+            return type_element_codes[type]
     for type in level_3:
         if type in types:
-            return type_names[type]
+            return type_element_codes[type]
     for type in level_4:
         if type in types:
-            return type_names[type]
-    request_data = {
-        'USER_ID': '311',
-        'MESSAGE': f'Типы {types}\nКомпания {company_id}'}
-    return 'Не найден'
+            return type_element_codes[type]
+    return '2355'
 
 
 def create_element(company_id, outgoing_email=False, connect_treatment=False, call_duration=False, incoming_call=False, outgoing_call_other=False):
@@ -190,7 +187,6 @@ def create_element(company_id, outgoing_email=False, connect_treatment=False, ca
     responsible = send_bitrix_request('crm.company.list', request_data)[0]['ASSIGNED_BY_ID']
 
     lk_call_count = '0'
-    total_interactions = '0'
     string_call_duration = '00:00:00'
     if call_duration:
         lk_call_count = '1'
@@ -211,9 +207,9 @@ def create_element(company_id, outgoing_email=False, connect_treatment=False, ca
             'PROPERTY_1359': int(outgoing_email),  # Исходящие письма
             'PROPERTY_1361': '1',  # Всего взаимодействий
             'PROPERTY_1365': int(connect_treatment),  # Обращений в 1С:Коннект
-            'PROPERTY_1367': sort_types(company_id),  # Топ сделка
             'PROPERTY_1369': int(incoming_call),   # Входящие звонки
             'PROPERTY_1375': int(outgoing_call_other),   # Исходящие (остальные)
+            'PROPERTY_1377': sort_types(company_id)      # Топ сделка
         }
     }
     element = send_bitrix_request('lists.element.add', request_data)
@@ -325,9 +321,9 @@ def update_element(company_id=None, element=None, outgoing_email=False, connect_
             'PROPERTY_1359': str(int(sent_emails) + outgoing_email),  # Исходящие письма
             'PROPERTY_1361': str(int(total_interactions) + total_interactions_bool),  # Всегда взаимодействий
             'PROPERTY_1365': str(int(connect_treatment_count) + connect_treatment),  # Обращений в 1С:Коннект
-            'PROPERTY_1367': top_deal,  # Топ сделка
             'PROPERTY_1369': str(int(incoming_calls) + incoming_call),     # Входящие звонки
             'PROPERTY_1375': str(int(outgoing_calls_others) + outgoing_call_other),     # Исходящие (остальные)
+            'PROPERTY_1377': top_deal  # Топ сделка
         }
     }
     send_bitrix_request('lists.element.update', request_data)

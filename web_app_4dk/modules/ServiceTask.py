65b36edc2e06ba3434245ec71f5992954b92f825
Max <m.k4rp0v@yandex.ru>
2022-12-01 16:26:50 +0300

    Update ServiceTask.py

--- web_app_4dk/modules/ServiceTask.py
+++ web_app_4dk/modules/ServiceTask.py
@@ -157,7 +157,6 @@ def get_quarter_deals_for_service_tasks(date_start, date_end, type_deals, employ
 
 
 def get_deals_for_service_tasks(date_start, date_end, type_deals, employees, stages):
-    print(date_start, date_end)
     """
     Функция, которая вызывается из функции create_task_service
 

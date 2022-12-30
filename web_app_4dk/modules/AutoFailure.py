efa3a7f7cc5af577f7c32fe181a8669e00365041
korpovmoxem <m.k4rp0v@yandex.ru>
2022-12-30 17:37:35 +0300

    Update AutoFailure.py

--- web_app_4dk/modules/AutoFailure.py
+++ web_app_4dk/modules/AutoFailure.py
@@ -2,8 +2,10 @@ from datetime import datetime
 
 from fast_bitrix24 import Bitrix
 
+from web_app_4dk.modules.authentication import authentication
 
-b = Bitrix('https://vc4dk.bitrix24.ru/rest/311/wkq0a0mvsvfmoseo/')
+
+b = Bitrix(authentication('Bitrix'))
 
 
 def auto_failure(req):

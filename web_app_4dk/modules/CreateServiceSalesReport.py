ce364a15cfe38eebf1cc85cb45ad3c02da193477
Max <m.k4rp0v@yandex.ru>
2023-01-13 16:44:34 +0300

    Update CreateServiceSalesReport.py

--- web_app_4dk/modules/CreateServiceSalesReport.py
+++ web_app_4dk/modules/CreateServiceSalesReport.py
@@ -157,7 +157,7 @@ def deal_info_handler(deals_info, users_info, month, edo_list_elements=None):
                 handled_data[deal_info['Ответственный']]['Сервисы'][f"{month} {deal_info['Тип']}"] += deal_value
 
             else:
-                if deal_info['Тип'] == '1Спарк 300' and month == 'Январь' and deal_info['Ответственный'] == 'Мария Скороходова':
+                if deal_info['Тип'] == '1Спарк 3000' and month == 'Январь' and deal_info['Ответственный'] == 'Мария Скороходова':
                     print(deal_info['ID'], deal_start_date)
                 if deal_start_date == f'{month_names_numbers[month]}.{str(months_and_years[month])}':
                     if deal_info['Тип'] == 'Подпись 1000':

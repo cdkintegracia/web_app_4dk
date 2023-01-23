57d049ff23cb6823473f32697b7c9d2ccd11b788
Max <m.k4rp0v@yandex.ru>
2023-01-23 14:47:53 +0300

    1

--- web_app_4dk/modules/CompleteDocumentFlowTask.py
+++ web_app_4dk/modules/CompleteDocumentFlowTask.py
@@ -9,8 +9,8 @@ b = Bitrix(authentication('Bitrix'))
 def complete_document_flow_task(req):
     company_name = req['company_name']
     task_name_search = f"Как будем обмениваться документами с {company_name}"
-    task = b.get_all('tasks.task.list', {'filter': {'TITLE': task_name_search}})
-    if task:
-        task = task[0]
-        b.call('tasks.task.update', {'taskId': task['id'], 'fields': {'STATUS': '5'}})
-        b.call('task.commentitem.add', [task['id'], {'POST_MESSAGE': 'Способ обмена заполнен в карточке компании', 'AUTHOR_ID': '173'}], raw=True)
+    tasks = b.get_all('tasks.task.list', {'filter': {'TITLE': task_name_search, '!STATUS': '5'}})
+    if tasks:
+        for task in tasks:
+            b.call('tasks.task.update', {'taskId': task['id'], 'fields': {'STATUS': '5'}})
+            b.call('task.commentitem.add', [task['id'], {'POST_MESSAGE': 'Способ обмена заполнен в карточке компании', 'AUTHOR_ID': '173'}], raw=True)

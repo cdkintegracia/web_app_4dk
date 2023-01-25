ec197dd09a0b032598a20bf0d52c306777f48866
Max <m.k4rp0v@yandex.ru>
2023-01-25 17:16:35 +0300

    Update AddInvoiceToList.py

--- web_app_4dk/modules/AddInvoiceToList.py
+++ web_app_4dk/modules/AddInvoiceToList.py
@@ -1,6 +1,6 @@
 from fast_bitrix24 import Bitrix
 
-from authentication import authentication
+from web_app_4dk.modules.authentication import authentication
 import requests
 
 b = Bitrix(authentication('Bitrix'))

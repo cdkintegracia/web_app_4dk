a1ec4dc6a3e4b8186ca79fbebae16519f9607a04
Max <m.k4rp0v@yandex.ru>
2023-01-27 15:26:43 +0300

    Delete fdsvds.py

--- web_app_4dk/modules/fdsvds.py
+++
@@ -1,8 +0,0 @@
-from fast_bitrix24 import Bitrix
-
-from authentication import authentication
-
-b = Bitrix(authentication('Bitrix'))
-
-a = b.get_all('crm.item.get', {'entityTypeId': '141', 'id': '343'})
-print(a)

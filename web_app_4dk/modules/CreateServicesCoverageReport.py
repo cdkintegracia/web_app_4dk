bf88ec8ca5ac20994a019f14e4e706b353a6620f
Max <m.k4rp0v@yandex.ru>
2023-02-02 11:49:43 +0300

    1

--- web_app_4dk/modules/CreateServicesCoverageReport.py
+++ web_app_4dk/modules/CreateServicesCoverageReport.py
@@ -69,6 +69,7 @@ def color_cells(report_name):
 
 
 def upload_report_to_bitrix(users_id, report_name, bitrix_folder_id='293499'):
+    '''
     # Загрузка отчета в Битрикс
     with open(report_name, 'rb') as file:
         report_file = file.read()
@@ -82,6 +83,8 @@ def upload_report_to_bitrix(users_id, report_name, bitrix_folder_id='293499'):
         b.call('im.notify.system.add', {
             'USER_ID': user_id,
             'MESSAGE': f'Отчет по охвату сервисами сформирован. {upload_report["DETAIL_URL"]}'})
+    '''
+    print(report_name)
     os.remove(report_name)
 
 

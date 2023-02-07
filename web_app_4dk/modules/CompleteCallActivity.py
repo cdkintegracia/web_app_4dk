2d29abae6a21ae97a4bd337d06aa8adba41b76c1
Max <m.k4rp0v@yandex.ru>
2023-02-07 13:31:24 +0300

    Update CompleteCallActivity.py

--- web_app_4dk/modules/CompleteCallActivity.py
+++ web_app_4dk/modules/CompleteCallActivity.py
@@ -1,17 +1,19 @@
-from fast_bitrix24 import Bitrix
+import requests
 
 from web_app_4dk.modules.authentication import authentication
-
-b = Bitrix(authentication('Bitrix'))
 
 
 def complete_call_activity(req):
     activity_id = req['data[FIELDS][ID]']
-    activity_info = b.get_all('crm.activity.list', {
+    req_data = {
         'filter': {
             'PROVIDER_TYPE_ID': 'CALL',
             'COMPLETED': 'N',
             'ID': activity_id
-        }})
+        }}
+    activity_info = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", data=req_data).json()['result']
+    print(activity_info)
+    '''
     if activity_info:
         b.get_all('crm.activity.update', {'id': activity_id, 'fields': {'COMPLETED': 'Y'}})
+    '''

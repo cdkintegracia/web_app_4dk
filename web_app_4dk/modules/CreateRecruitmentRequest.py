644464b9c30a3be720fcfb85e8ecc4e172568075
Max <m.k4rp0v@yandex.ru>
2023-02-08 17:40:37 +0300

    Update CreateRecruitmentRequest.py

--- web_app_4dk/modules/CreateRecruitmentRequest.py
+++ web_app_4dk/modules/CreateRecruitmentRequest.py
@@ -10,9 +10,9 @@ def create_recruitment_request(req):
     department_info = b.get_all('department.get', {'ID': req['department']})
     department_name = department_info[0]['NAME']
     b.call('crm.item.add', {
-        'entityTypeId': '130', {
-            'fields': {
-                'UF_CRM_15_1655883348': req['responsible'][5:],
-                'UF_CRM_15_1655883493': req['file_id'],
-                'UF_CRM_15_1655883421': department_name,
-            }}})
+        'entityTypeId': '130',
+        'fields': {
+            'UF_CRM_15_1655883348': req['responsible'][5:],
+            'UF_CRM_15_1655883493': req['file_id'],
+            'UF_CRM_15_1655883421': department_name,
+            }})

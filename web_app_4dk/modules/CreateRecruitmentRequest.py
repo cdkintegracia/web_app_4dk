9b019a80bea5e178bf25204005b308001a4172c9
Max <m.k4rp0v@yandex.ru>
2023-02-08 17:58:44 +0300

    Update CreateRecruitmentRequest.py

--- web_app_4dk/modules/CreateRecruitmentRequest.py
+++ web_app_4dk/modules/CreateRecruitmentRequest.py
@@ -15,6 +15,6 @@ def create_recruitment_request(req):
         'fields': {
             'TITLE': f"{req['responsible'][5:]} {department_name} {req['file_id']}",
             'ufCrm15_1655883348': req['responsible'][5:],
-            #'UF_CRM_15_1655883493': req['file_id'],
+            'ufCrm15_1655883493': req['file_id'],
             #'UF_CRM_15_1655883421': department_name,
             }})

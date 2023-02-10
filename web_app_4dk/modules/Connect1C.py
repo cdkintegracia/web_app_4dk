975ba08af0d8045b160cac50e4bb69770cb20589
Max <m.k4rp0v@yandex.ru>
2023-02-10 17:19:05 +0300

    Update Connect1C.py

--- web_app_4dk/modules/Connect1C.py
+++ web_app_4dk/modules/Connect1C.py
@@ -327,7 +327,6 @@ def connect_1c(req: dict):
     with open('/root/web_app_4dk/web_app_4dk/static/logs/connect_logs.txt', 'a') as file:
         json.dump(req, file, ensure_ascii=False)
         file.write('\n')
-    write_logs_to_database(req)
 
     # Новое обращение
     if req['message_type'] == 80:

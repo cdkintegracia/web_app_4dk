7a7cbeda2a6bf9110900153092a87c64fe229371
Max <m.k4rp0v@yandex.ru>
2023-02-10 16:33:16 +0300

    1

--- web_app_4dk/modules/CreateServiceSalesReport.py
+++ web_app_4dk/modules/CreateServiceSalesReport.py
@@ -139,9 +139,6 @@ def deal_info_handler(deals_info, users_info, month, edo_list_elements=None):
         if type(deal_info['Предполагаемая дата закрытия']) == str:
             deal_info['Предполагаемая дата закрытия'] = datetime.strptime(deal_info['Предполагаемая дата закрытия'], '%d.%m.%Y')
 
-        if deal_info['ID'] in ['93773', '89703']:
-            print(deal_info['ID'], deal_info['Тип'], deal_info['Стадия сделки'], deal_info['Тип'] in service_deal_types, deal_info['Стадия сделки'] == 'Услуга активна')
-
         if deal_info['Группа'] == 'ИТС' and deal_info['Стадия сделки'] in ['Услуга активна', 'Счет сформирован', 'Счет отправлен клиенту'] and 'ГРМ' not in deal_info['Тип']:
             handled_data[deal_info['Ответственный']][its_deal_value_field] += 1
 

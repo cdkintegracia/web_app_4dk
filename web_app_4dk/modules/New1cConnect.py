bcb8230d118b0175789a70007f6ed66b68014cd4
Max <m.k4rp0v@yandex.ru>
2023-02-17 16:07:04 +0300

    1

--- web_app_4dk/modules/New1cConnect.py
+++ web_app_4dk/modules/New1cConnect.py
@@ -674,6 +674,9 @@ def connect_1c_event_handler(req):
         )
         with connect:
             result = connect.execute(sql, data).fetchone()
+        b.call('im.notify.system.add', {
+            'USER_ID': '311',
+            'MESSAGE': f"Юзер АЙДИ: {author_bitrix_id}', {req['treatment_id']} {result}"})
         responsible_id = result[0]
         task_id = result[1]
         if responsible_id != author_bitrix_id:

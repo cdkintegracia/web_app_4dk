872548aeb0f0520008ec60aef09e7a150159e691
Max <m.k4rp0v@yandex.ru>
2023-02-21 12:30:23 +0300

    Create CheckProductNomenclature.py

---
+++ web_app_4dk/modules/CheckProductNomenclature.py
@@ -0,0 +1,10 @@
+from fast_bitrix24 import Bitrix
+
+try:
+    from authentication import authentication
+except ModuleNotFoundError:
+    from web_app_4dk.modules.authentication import authentication
+
+
+def check_product_nomenclature(req):
+    print(req)

fc861deb9e6a5532183c1164daae4f221c70c5a2
Max <m.k4rp0v@yandex.ru>
2023-02-21 12:44:07 +0300

    1

--- web_app_4dk/modules/CheckProductNomenclature.py
+++ web_app_4dk/modules/CheckProductNomenclature.py
@@ -1,3 +1,5 @@
+import builtins
+
 from fast_bitrix24 import Bitrix
 
 try:
@@ -6,6 +8,17 @@ except ModuleNotFoundError:
     from web_app_4dk.modules.authentication import authentication
 
 
+b = Bitrix(authentication('Bitrix'))
+
+
 def check_product_nomenclature(req):
-    products = req['products']
-    print(products)
+    product_id = req['product']
+    product_info = b.get_all('crm.item.productrow.get', {'id': product_id})
+    product_id = product_info['productRow']['productId']
+    product_info = b.get_all('crm.product.get', {'id': product_id})
+    if product_info['SECTION_ID'] not in ['219', '331']:
+        b.call('im.notify.system.add', {
+            'USER_ID': req['user_id'][5:],
+            'MESSAGE': f'Используйте правильную номенклатуру для формирования договора ИТС'})
+        return
+    

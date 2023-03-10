73c7925ab5bc65e9203b6c06a9fff78c12a89952
korpovmoxem <m.k4rp0v@yandex.ru>
2023-03-10 16:52:47 +0300

    Update FillTaskTitle.py

--- web_app_4dk/modules/FillTaskTitle.py
+++ web_app_4dk/modules/FillTaskTitle.py
@@ -12,7 +12,13 @@ b = Bitrix(authentication('Bitrix'))
 def fill_task_title(req):
     task_id = req['data[FIELDS_AFTER][ID]']
     task_info = b.get_all('tasks.task.get', {'taskId': task_id, 'select': ['TITLE', 'UF_CRM_TASK']})['task']
-    print(task_info)
+    uf_crm_task = ''
+    for crm_id in task_info['ufCrmTask']:
+        if 'CO' in crm_id:
+            uf_crm_task = crm_id[:3]
+            break
+    print(task_info['ufCrmTask'])
+    print(uf_crm_task)
 
 
 

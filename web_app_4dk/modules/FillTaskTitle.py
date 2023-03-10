bea8ecbbe1c9cf0583ab17fbb81b98015f71c0b8
korpovmoxem <m.k4rp0v@yandex.ru>
2023-03-10 17:11:27 +0300

    Update FillTaskTitle.py

--- web_app_4dk/modules/FillTaskTitle.py
+++ web_app_4dk/modules/FillTaskTitle.py
@@ -13,7 +13,8 @@ def fill_task_title(req):
     task_id = req['data[FIELDS_AFTER][ID]']
     task_info = b.get_all('tasks.task.get', {
         'taskId': task_id,
-        'select': ['TITLE', 'UF_CRM_TASK']})['task']
+        'select': ['TITLE', 'UF_CRM_TASK']
+    })['task']
     if not task_info['ufCrmTask']:
         return
     company_crm = list(filter(lambda x: 'CO' in x, task_info['ufCrmTask']))
@@ -23,17 +24,13 @@ def fill_task_title(req):
     company_info = b.get_all('crm.company.get', {
         'ID': company_id,
     })
-    print(company_info)
-    exit()
     if company_info['TITLE'] in task_info['TITLE']:
         return
-    '''
     b.call('tasks.task.update', {
         'taskId': task_id,
         'fields': {
             'TITLE': f"{task_info['TITLE']} {company_info['TITLE']}"
         }})
-    '''
 
 
 

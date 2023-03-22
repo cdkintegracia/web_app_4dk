2a3768440f1ea48287c6e45db27d1b9f617fa78f
Max <m.k4rp0v@yandex.ru>
2023-03-22 16:49:53 +0300

    1

--- web_app_4dk/modules/TaskHandler.py
+++ web_app_4dk/modules/TaskHandler.py
@@ -8,19 +8,19 @@ def fill_task_title(req):
         'select': ['TITLE', 'UF_CRM_TASK', 'AUDITORS']
     })
     if not task_info or not task_info['task']['ufCrmTask']:
-        return task_info
+        return
 
     task_info = task_info['task']
     company_crm = list(filter(lambda x: 'CO' in x, task_info['ufCrmTask']))
     if not company_crm:
-        return task_info
+        return
 
     company_id = company_crm[0][3:]
     company_info = send_bitrix_request('crm.company.get', {
         'ID': company_id,
     })
     if company_info['TITLE'] in task_info['title']:
-        return task_info
+        return
 
     send_bitrix_request('tasks.task.update', {
         'taskId': task_id,
@@ -31,12 +31,14 @@ def fill_task_title(req):
 
 
 def send_notification(task_info):
-    print(task_info)
+    users_notification_list = ['311']
     if not task_info['task'] or not task_info['task']['auditors']:
         return
     auditors = task_info['task']['auditors']
-    users_notification_list = ['311']
-    send_bitrix_request('im.notify.system.add', {'USER_ID': '311', 'MESSAGE': auditors})
+    task_id = task_info['task']['id']
+    for user in users_notification_list:
+        if user in auditors:
+            send_bitrix_request('im.notify.system.add', {'USER_ID': '311', 'MESSAGE': f"https://vc4dk.bitrix24.ru/company/personal/user/{user}/tasks/task/view/{task_id}/"})
 
 
 def task_handler(req):

e5fb3009f666108a823407bc3e5951dd64677faa
Max <m.k4rp0v@yandex.ru>
2023-03-22 16:43:22 +0300

    Update TaskHandler.py

--- web_app_4dk/modules/TaskHandler.py
+++ web_app_4dk/modules/TaskHandler.py
@@ -31,6 +31,7 @@ def fill_task_title(req):
 
 
 def send_notification(task_info):
+    print(task_info)
     if not task_info['task'] or not task_info['task']['AUDITORS']:
         return
     auditors = task_info['task']['AUDITORS']

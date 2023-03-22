26861035aba6eea82b4c3ddda09276edfa25da11
Max <m.k4rp0v@yandex.ru>
2023-03-22 16:31:06 +0300

    Update CreateCurrentMonthDealsDataFile.py

--- web_app_4dk/modules/CreateCurrentMonthDealsDataFile.py
+++ web_app_4dk/modules/CreateCurrentMonthDealsDataFile.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fast_bitrix24 import Bitrix
 import openpyxl
 
-from web_app_4dk.modules.TaskHandler import deals_category_1_types, deals_category_1_stage_ids, UF_CRM_1657878818384_values
+from web_app_4dk.modules.field_values import deals_category_1_types, deals_category_1_stage_ids, UF_CRM_1657878818384_values
 from web_app_4dk.modules.authentication import authentication
 
 

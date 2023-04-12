14b82733447c7afb24b4a59d7d958fa5cf0f6b55
Max <m.k4rp0v@yandex.ru>
2023-04-12 14:32:31 +0300

    Update ChangeTaskGroup.py

--- web_app_4dk/modules/ChangeTaskGroup.py
+++ web_app_4dk/modules/ChangeTaskGroup.py
@@ -7,11 +7,10 @@ b = Bitrix(authentication('Bitrix'))
 
 
 def change_task_group(req):
-    print(req['responsible_id'][5:])
     groups_and_departments = {
         '9': [361, 225],
     }
-    user_info = b.get_all('user.get', {'filter': {'ID': req['responsible_id'][:5]}})[0]
+    user_info = b.get_all('user.get', {'filter': {'ID': req['responsible_id'][5:]}})[0]
     for group in groups_and_departments:
         for department in groups_and_departments[group]:
             if department in user_info['UF_DEPARTMENT']:

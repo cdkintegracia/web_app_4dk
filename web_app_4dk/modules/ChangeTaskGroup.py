bc5129f541779b4437d7aaaf8218fe994037d122
Max <m.k4rp0v@yandex.ru>
2023-04-12 14:30:52 +0300

    Update ChangeTaskGroup.py

--- web_app_4dk/modules/ChangeTaskGroup.py
+++ web_app_4dk/modules/ChangeTaskGroup.py
@@ -11,7 +11,7 @@ def change_task_group(req):
     groups_and_departments = {
         '9': [361, 225],
     }
-    user_info = b.get_all('user.get', {'filter': {'ID': req['responsible_id']}})[0]
+    user_info = b.get_all('user.get', {'filter': {'ID': req['responsible_id'][:5]}})[0]
     for group in groups_and_departments:
         for department in groups_and_departments[group]:
             if department in user_info['UF_DEPARTMENT']:

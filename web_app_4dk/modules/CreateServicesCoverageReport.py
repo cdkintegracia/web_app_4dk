0f8488452b11384f218861211fb058f64c472ecd
Max <m.k4rp0v@yandex.ru>
2023-04-13 16:48:47 +0300

    Update CreateServicesCoverageReport.py

--- web_app_4dk/modules/CreateServicesCoverageReport.py
+++ web_app_4dk/modules/CreateServicesCoverageReport.py
@@ -216,9 +216,6 @@ def create_services_coverage_report(req):
     for company_id in result_data:
         for data_counter in range(len(result_data[company_id])):
             deals_reporting = list(filter(lambda x: x['TYPE_ID'] in ['UC_O99QUW', 'UC_OV4T7K'] and x['UF_CRM_1640523562691'] == result_data[company_id][data_counter]['Регномер'], deals_info))
-            if company_id == '1413':
-                print(len(deals_reporting))
-                exit()
             for deal_reporting in deals_reporting:
                 if deal_reporting['ID'] not in deals_reporting_id:
                     if deal_reporting['TYPE_ID'] == 'UC_O99QUW':
@@ -226,6 +223,9 @@ def create_services_coverage_report(req):
                         deals_reporting_id.append(deal_reporting['ID'])
                 if deal_reporting['UF_CRM_1640523562691'] != result_data[company_id][data_counter]['Регномер']:
                     deals_reporting_by_regnumber = list(filter(lambda x: x['UF_CRM_1640523562691'] == deal_reporting['UF_CRM_1640523562691'] and x['TYPE_ID'] == 'UC_O99QUW', deals_info))
+                    if company_id == '1413':
+                        print(len(deals_reporting))
+                        exit()
                     for deal_reporting_by_regnumber in deals_reporting_by_regnumber:
                         if deal_reporting_by_regnumber['ID'] not in deals_reporting_id:
                             result_data[company_id][data_counter]['Отчетность'] += 1

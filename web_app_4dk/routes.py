9e2e52729a272f570bc1e2ee3d457d143d6c64e0
Max <m.k4rp0v@yandex.ru>
2023-04-17 18:10:39 +0300

    Update routes.py

--- web_app_4dk/routes.py
+++ web_app_4dk/routes.py
@@ -212,7 +212,7 @@ def main_page():
             edo_info_handler(month, year, '/root/web_app_4dk/web_app_4dk/edo_info_handler_file.xlsx')
             os.remove('/root/web_app_4dk/web_app_4dk/edo_info_handler_file.xlsx')
 
-    print(session)
+    print(session['_user_id'])
     return render_template('main_page.html', web_app_logs=read_logs())
 
 

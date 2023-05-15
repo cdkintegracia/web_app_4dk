3878ebc6bcb3195f41080bcb3e7395f472428f30
Max <m.k4rp0v@yandex.ru>
2023-05-15 14:24:56 +0300

    1

--- web_app_4dk/tools.py
+++ web_app_4dk/tools.py
@@ -10,4 +10,4 @@ def send_bitrix_request(method: str, data: dict):
     try:
         return requests.post(f"{authentication('Bitrix')}{method}", json=data).json()['result']
     except KeyError:
-        return
+        return 

bd6f6bd23592ba21cdca799a53ee4c0e27c5a0c7
Max <m.k4rp0v@yandex.ru>
2023-05-16 12:19:50 +0300

    1

--- web_app_4dk/modules/authentication.py
+++ web_app_4dk/modules/authentication.py
@@ -5,6 +5,7 @@ def authentication(key):
     credentials = {
         'Bitrix': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzMxMS83OG5vdXZ3ejlkcnNvbnkwLw==',
         'Google': 'Yml0cml4MjQtZGF0YS1zdHVkaW8tMjI3OGM3YmZiMWE3Lmpzb24=',
+        'Chat-bot': 'aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzMxMS81d2UyNzJ6YXRwamJkcXNsLw=='
     }
     return base64.b64decode(credentials[key]).decode('utf-8')
 

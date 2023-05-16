2fc5a8ea768d67f95c0be101958a49d9f60877a0
Max <m.k4rp0v@yandex.ru>
2023-05-16 13:22:34 +0300

    Update ChatBotTools.py

--- web_app_4dk/chat_bot_routes/ChatBotTools.py
+++ web_app_4dk/chat_bot_routes/ChatBotTools.py
@@ -9,4 +9,4 @@ class Message:
         self.skip_url_index = request_data['data[PARAMS][SKIP_URL_INDEX]']
         self.template_id = request_data['data[PARAMS][TEMPLATE_ID]']
         self.to_user_id = request_data['data[PARAMS][TO_USER_ID]']
-        self.event = request_data['ONIMBOTMESSAGEADD']
+        self.event = request_data['event']

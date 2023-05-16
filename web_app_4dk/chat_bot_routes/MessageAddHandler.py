7975f959621294535f1748c3df93514460c6693b
Max <m.k4rp0v@yandex.ru>
2023-05-16 13:01:57 +0300

    Update MessageAddHandler.py

--- web_app_4dk/chat_bot_routes/MessageAddHandler.py
+++ web_app_4dk/chat_bot_routes/MessageAddHandler.py
@@ -4,4 +4,4 @@ from web_app_4dk.chat_bot_routes.SendMessage import bot_send_message
 
 def message_add_handler(req):
     message = Message(req)
-    bot_send_message(message.from_user_id, message.text)
+    bot_send_message({'dialog_id': message.from_user_id, 'message': message.text})

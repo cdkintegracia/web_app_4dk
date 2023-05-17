a79bbb5c4fc9809da3ba7fd48166c6bd04e758dd
Max <m.k4rp0v@yandex.ru>
2023-05-17 16:44:37 +0300

    1

--- web_app_4dk/chat_bot/CommandsHandler.py
+++ web_app_4dk/chat_bot/CommandsHandler.py
@@ -6,14 +6,16 @@ from web_app_4dk.chat_bot.SendMessage import bot_send_message
 
 def command_add_handler(message):
     data = {
+        'BOT_ID': '495',
+        'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
+        'DIALOG_ID': '311',
+        'MESSAGE': 'Клавиатура',
         'COMMAND_ID': '39',
         'COMMAND': 'commands',
         'MESSAGE_ID': message.message_id,
-        'MESSAGE': 'Отвечаю',
-        'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
         'KEYBOARD': [{
             "TEXT": "Bitrix24",
-            "LINK":"http://bitrix24.com",
+            "LINK": "http://bitrix24.com",
             "BG_COLOR": "#29619b",
             "TEXT_COLOR": "#fff",
             "DISPLAY": "LINE",
--- web_app_4dk/chat_bot/выв.py
+++ web_app_4dk/chat_bot/выв.py
@@ -1,6 +1,6 @@
 import requests
 
-
+'''
 data = {
         'BOT_ID': '495',
         'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
@@ -14,4 +14,31 @@ data = {
 
 
 r = requests.post(url=f'https://vc4dk.bitrix24.ru/rest/311/5we272zatpjbdqsl/imbot.command.register', json=data)
+print(r.text)
+'''
+data = {
+        'BOT_ID': '495',
+        'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
+        'COMMAND': 'commands',
+        'COMMON': 'Y',
+        'COMMAND_ID': '37',
+        'LANG': ['RU', 'ENG'],
+        'EVENT_COMMAND_ADD': 'http://141.8.195.67:5000/bitrix/chat_bot',
+        'HIDDEN': 'N'
+    }
+
+data = {
+        'BOT_ID': '495',
+        'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
+        'DIALOG_ID': '311',
+        'MESSAGE': 'Клавиатура',
+        'KEYBOARD': [{ "TEXT": "Bitrix24",
+        "LINK": "http://bitrix24.com",
+        "BG_COLOR": "#29619b",
+        "TEXT_COLOR": "#fff",
+        "DISPLAY": "LINE",
+        }]
+    }
+
+r = requests.post(url=f'https://vc4dk.bitrix24.ru/rest/311/5we272zatpjbdqsl/imbot.message.add', json=data)
 print(r.text)

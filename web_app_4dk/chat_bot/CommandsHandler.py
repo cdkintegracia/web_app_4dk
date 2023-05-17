f161b11dd6c0435c29251119f5141f4efa7e17de
Max <m.k4rp0v@yandex.ru>
2023-05-17 16:24:33 +0300

    Update CommandsHandler.py

--- web_app_4dk/chat_bot/CommandsHandler.py
+++ web_app_4dk/chat_bot/CommandsHandler.py
@@ -12,3 +12,4 @@ def command_add_handler(message):
         'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
     }
     r = requests.post(url=f'{authentication("Chat-bot")}imbot.command.answer', json=data)
+    print(r.text)

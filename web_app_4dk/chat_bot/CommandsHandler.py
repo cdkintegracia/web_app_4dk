a3c37712f64f17d7133a6ba3f0cbc62046685038
Max <m.k4rp0v@yandex.ru>
2023-05-17 16:53:45 +0300

    1

--- web_app_4dk/chat_bot/CommandsHandler.py
+++ web_app_4dk/chat_bot/CommandsHandler.py
@@ -10,6 +10,15 @@ def command_add_handler(message):
         'MESSAGE_ID': message.message_id,
         'MESSAGE': 'Отвечаю',
         'CLIENT_ID': 'vv58t6uleb5nyr3li47xp2mj5r3n46tb',
+        'KEYBOARD': [
+            {
+                'TEXT': "Bitrix24",
+                'LINK': "http://bitrix24.com",
+                'BG_COLOR': "#29619b",
+                'TEXT_COLOR': "#fff",
+                'DISPLAY': 'LINE'
+            }
+        ]
     }
     r = requests.post(url=f'{authentication("Chat-bot")}imbot.command.answer', json=data)
     print(r.text)

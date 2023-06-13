3f8c55e8ab7fe366f33c3c8a2597f04440f2658a
Max <m.k4rp0v@yandex.ru>
2023-06-13 11:44:53 +0300

    1

--- web_app_4dk/routes.py
+++ web_app_4dk/routes.py
@@ -122,6 +122,7 @@ bot_custom_webhooks = {
 def default_webhook():
     #update_logs("Получен дефолтный вебхук", request.form)
     if request.form['event'] == 'ONTASKADD':
+        print(request)
         default_webhooks[request.form['event']](request.form, event='ONTASKADD')
     else:
         default_webhooks[request.form['event']](request.form)

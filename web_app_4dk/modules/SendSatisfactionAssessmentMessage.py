5b843e69f3f806e34a4c0ac8c8b49b26c9614136
Max <m.k4rp0v@yandex.ru>
2023-06-15 14:48:48 +0300

    1

--- web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
+++ web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
@@ -15,7 +15,24 @@ def send_satisfaction_assessment_message(req):
     if not contact_id:
         return
     contact_id = contact_id[0][2:]
+    contact_info = b.get_all('crm.contact.get', {'ID': contact_id, 'select': ['PHONE']})
+    contact_phones = list(map(lambda x: x['VALUE'], contact_info['PHONE']))
+    calls = b.get_all('voximplant.statistic.get', {
+        'filter': {
+            'CRM_ENTITY_TYPE': 'CONTACT',
+            'PHONE_NUMBER': contact_phones,
+            '>CALL_START_DATE': task_info['createdDate'],
+            'CALL_FAILED_CODE': '200',
+            'PORTAL_USER_ID': task_info['responsibleId'],
+            'CALL_TYPE': '1',
 
+        }})
+    if not calls:
+        return
+    call_phone_number = calls[0]['PHONE_NUMBER']
+    if call_phone_number[2:5] == in ['812', '812']:
+        return
+    print(call_phone_number[2:5])
 
 
 data = {

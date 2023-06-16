be8b3624a56f9183af2aa2f516dbc24f3f53c73e
Max <m.k4rp0v@yandex.ru>
2023-06-16 14:39:00 +0300

    Update SendDealChangedUserMessage.py

--- web_app_4dk/chat_bot/SendDealChangedUserMessage.py
+++ web_app_4dk/chat_bot/SendDealChangedUserMessage.py
@@ -2,4 +2,7 @@ from web_app_4dk.chat_bot.SendMessage import bot_send_message
 
 
 def send_deal_changed_user_message(req):
-    print(req)
+    bot_send_message({
+        'dialog_id': '311',
+        'message': f'Ответственным за сделку {req["deal_name"]} https://vc4dk.bitrix24.ru/crm/deal/details/{req["ID"]}/ был назначен {req["user_after"]} вместо {req["user_before"]}'
+    })

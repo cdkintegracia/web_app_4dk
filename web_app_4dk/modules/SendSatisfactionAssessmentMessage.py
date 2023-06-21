7a087ee8aff6bcdd136fea337b38765e96d1a68e
Max <m.k4rp0v@yandex.ru>
2023-06-21 16:04:05 +0300

    1

--- web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
+++ web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
@@ -13,30 +13,25 @@ def send_satisfaction_assessment_message(req):
     task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})['task']
     contact_id = list(filter(lambda x: 'C_' in x, task_info['ufCrmTask']))
     if not contact_id:
-        print('ЗАДАЧА НЕ СОЗДАНА - НЕ НАЙДЕН КОНТАКТ')
         return
     contact_id = contact_id[0][2:]
     contact_info = b.get_all('crm.contact.get', {'ID': contact_id, 'select': ['PHONE']})
     contact_phones = list(map(lambda x: x['VALUE'], contact_info['PHONE']))
-    print(contact_phones)
     calls = b.get_all('voximplant.statistic.get', {
         'filter': {
-            #'CRM_ENTITY_TYPE': 'CONTACT',
+            'CRM_ENTITY_TYPE': 'CONTACT',
             'PHONE_NUMBER': contact_phones,
             '>CALL_START_DATE': task_info['createdDate'],
-            #'CALL_FAILED_CODE': '200',
-            #'PORTAL_USER_ID': task_info['responsibleId'],
-            #'CALL_TYPE': '1',
+            'CALL_FAILED_CODE': '200',
+            'PORTAL_USER_ID': task_info['responsibleId'],
+            'CALL_TYPE': '1',
 
         }})
-    print(calls)
-    exit()
+
     if not calls:
-        print('ЗАДАЧА НЕ СОЗДАНА - НЕ НАЙДЕН ЗВОНОК')
         return
     call_phone_number = calls[0]['PHONE_NUMBER']
     if call_phone_number[2:5] in ['812', '812']:
-        print('ЗАДАЧА НЕ СОЗДАНА - НАЙДЕН ГОРОДСКОЙ НОМЕР')
         return
 
     b.call('crm.item.add', {

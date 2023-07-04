c05b6512f269927aa8f584d6c9734d4b4d239a73
Max <m.k4rp0v@yandex.ru>
2023-07-04 15:47:39 +0300

    1

--- web_app_4dk/modules/CheckTaskResult.py
+++ web_app_4dk/modules/CheckTaskResult.py
@@ -68,5 +68,5 @@ def check_task_result(dct):
             check_list = b.call('task.checklistitem.getlist', [main_task_id], raw=True)['result']
             task_row = list(filter(lambda x: dct['id'] in x['TITLE'], check_list))
             print(task_row)
-            b.call('task.checklistitem.complete', [task_row[0]['PARENT_ID'], task_row[0]['ID']])
+            b.call('task.checklistitem.complete', [task_row[0]['PARENT_ID'], task_row[0]['ID']], raw=True)
 

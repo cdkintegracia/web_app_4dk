4814e7f9cdda2c72e0395df42a561e30059de216
Max <m.k4rp0v@yandex.ru>
2023-07-06 13:23:30 +0300

    1

--- web_app_4dk/modules/CompleteDocumentFlowTask.py
+++ web_app_4dk/modules/CompleteDocumentFlowTask.py
@@ -8,9 +8,12 @@ b = Bitrix(authentication('Bitrix'))
 
 def complete_document_flow_task(req):
     company_name = req['company_name']
-    task_name_search = f"Как будем обмениваться документами с {company_name}"
+    task_name_search = f"Как будем обмениваться документами с {company_name.strip()}"
     tasks = b.get_all('tasks.task.list', {'filter': {'TITLE': task_name_search, '!STATUS': '5'}})
     if tasks:
         for task in tasks:
             b.call('tasks.task.update', {'taskId': task['id'], 'fields': {'STATUS': '5'}})
             b.call('task.commentitem.add', [task['id'], {'POST_MESSAGE': 'Способ обмена заполнен в карточке компании', 'AUTHOR_ID': '173'}], raw=True)
+
+
+complete_document_flow_task({'company_name': 'ПЛАНЕТА ЗДОРОВОЙ КОЖИ 4706061700'})

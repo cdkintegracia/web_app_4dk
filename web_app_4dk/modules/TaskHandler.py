9130ec4dc67aee00c3e5c30a6039c9e66fb4c256
Max <m.k4rp0v@yandex.ru>
2023-07-18 11:20:18 +0300

    Update TaskHandler.py

--- web_app_4dk/modules/TaskHandler.py
+++ web_app_4dk/modules/TaskHandler.py
@@ -124,7 +124,7 @@ def fill_task_title(req, event):
     if not task_info or 'task' not in task_info or not task_info['task']:
         return
     task_info = task_info['task']
-    task_registry(task_info)
+    task_registry(task_info, event)
     '''
     if task_info['closedDate'] and task_info['ufAuto934103382947'] != '1':
         send_notification(task_info, 'Завершение')

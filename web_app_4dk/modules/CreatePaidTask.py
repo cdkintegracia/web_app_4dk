7cc882a4bf69ac9cf078f047902d641770dbb361
Max <m.k4rp0v@yandex.ru>
2023-07-21 13:55:01 +0300

    1

--- web_app_4dk/modules/CreatePaidTask.py
+++ web_app_4dk/modules/CreatePaidTask.py
@@ -7,4 +7,20 @@ b = Bitrix(authentication('Bitrix'))
 
 
 def create_paid_task(req):
-    
+    b.call('tasks.task.add', {
+        'fields': {
+            'TITLE': 'Платная задача завершена! Выставьте счет клиенту.',
+            'DESCRIPTION': f'Завершена платная задача.\n'
+                           f'Ссылка на задачу: {req["task_url"]}\n\n'
+                           f'Информация о задаче:\n'
+                           f'Группа: {req["group"]}\n'
+                           f'Название: {req["title"]}\n'
+                           f'Описание: {req["description"]}\n'
+                           f'Статус: {req["status"]}\n'
+                           f'Постановщик: {req["created_by"]}\n'
+                           f'Ответственный: {req["responsible"]}\n'
+                           f'Дата создания: {req["created_date"]}\n'
+                           f'Дата закрытия: {req["close_date"]}\n'
+                           f'Трудозатраты: {req["time"]}'
+        }
+    })

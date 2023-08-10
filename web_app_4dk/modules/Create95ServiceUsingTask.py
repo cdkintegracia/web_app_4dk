0e93a39856708f01afc53c1485d7dbc5d6130f9d
Max <m.k4rp0v@yandex.ru>
2023-08-10 13:47:05 +0300

    Update Create95ServiceUsingTask.py

--- web_app_4dk/modules/Create95ServiceUsingTask.py
+++ web_app_4dk/modules/Create95ServiceUsingTask.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 from fast_bitrix24 import Bitrix
 
 from web_app_4dk.modules.authentication import authentication
+from web_app_4dk.tools import send_bitrix_request
 
 
 b = Bitrix(authentication('Bitrix'))
@@ -8,9 +11,21 @@ b = Bitrix(authentication('Bitrix'))
 
 def create_95_service_using_task(req):
     task_title = f"Для {req['company_name']} использование сервиса {req['service_name']} достигло 95% лимита"
-    tasks = b.get_all('tasks.task.list', {
+    filter_date_start = datetime.now() - timedelta(days=1)
+    filter_date_end = datetime.now() + timedelta(days=1)
+    tasks = send_bitrix_request('tasks.task.list', {
         'filter': {
-            'TITLE': task_title
+            'TITLE': task_title,
+            '>CREATED_DATE': filter_date_start,
+            '<CREATED_DATE': filter_date_end
         }
     })
-    print(tasks[0])
+    if not tasks:
+        send_bitrix_request('tasks.task.add', {
+            'fields': {
+                'TITLE': task_title,
+                'CREATED_BY': '173',
+                'RESPONSIBLE_ID': '133',
+                'UF_CRM_TASK': ['CO_' + req['company_id']]
+            }
+        })

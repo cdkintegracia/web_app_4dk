8104a797ef5e7a0359edb986239346564b28847f
Max <m.k4rp0v@yandex.ru>
2023-08-30 17:14:25 +0300

    1

--- web_app_4dk/routes.py
+++ web_app_4dk/routes.py
@@ -211,7 +211,7 @@ def send_service_coverage_report_to_employees():
 
 @app.route('/send_request_fill_act_document_smart_process', methods=['GET'])
 def route_send_request_fill_document_smart_process():
-    fill_act_document_smart_process({'user_id': '311'})
+    fill_act_document_smart_process({'user_id': 'user_311'})
     return 'OK'
 
 

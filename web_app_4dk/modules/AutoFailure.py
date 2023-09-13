93dcd4e1a39ff196f1d5538b310c7ce7360ee98c
Max <m.k4rp0v@yandex.ru>
2023-09-13 10:14:53 +0300

    Update AutoFailure.py

--- web_app_4dk/modules/AutoFailure.py
+++ web_app_4dk/modules/AutoFailure.py
@@ -16,7 +16,6 @@ def auto_failure(req):
     filter_date_end = datetime(day=1, month=filter_date.month + 2, year=filter_date.year) - timedelta(days=2)
     filter_date_end = datetime.strftime(filter_date_end, '%Y-%m-%d')
     filter_date_start = datetime.strftime(filter_date, '%Y-%m-%d')
-    print(filter_date_start)
     deal_types = [
         'UC_HT9G9H',  # ПРОФ Земля
         'UC_XIYCTV',  # ПРОФ Земля+Помощник
@@ -33,7 +32,8 @@ def auto_failure(req):
                    'UF_CRM_1637933869479': '1',
                    'TYPE_ID': deal_types
                    }})
-    companies = b.get_all('crm.company.list', {'filter': {'ID': list(map(lambda x: x['COMPANY_ID'], deals))}})
+    if deals:
+        companies = b.get_all('crm.company.list', {'filter': {'ID': list(map(lambda x: x['COMPANY_ID'], deals))}})
 
     for index, deal in enumerate(deals, 1):
         b.call('bizproc.workflow.start', {'TEMPLATE_ID': '759', 'DOCUMENT_ID': ['crm', 'CCrmDocumentDeal', 'DEAL_' + deal['ID']]})

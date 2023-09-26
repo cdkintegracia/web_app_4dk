9178bc11ca1ff98c0bf1f5ef117ef1ad5a3cabcb
Max <m.k4rp0v@yandex.ru>
2023-09-26 17:55:28 +0300

    1

---
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -0,0 +1,139 @@
+from datetime import datetime, timedelta
+import base64
+import os
+
+import openpyxl
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill, Font
+from fast_bitrix24 import Bitrix
+
+from web_app_4dk.modules.authentication import authentication
+
+
+b = Bitrix(authentication('Bitrix'))
+
+
+def get_employee_id(users: str) -> list:
+    """
+    Приводит строку с id пользователей и id подразделений к единому списку, состоящему только из id сотрудников
+
+    :param users: Строка из параметра запроса Б24 состоящая из {user_...} и|или {group_...}, которые разделены ', '
+    """
+
+    users_id_set = set()
+
+    # Строка с сотрудниками и отделами в список
+    users = users.split(', ')
+
+    # Если в массиве найден id сотрудника
+    for user_id in users:
+        if 'user' in user_id:
+            users_id_set.add(user_id[5:])
+
+        # Если в массиве найден id отдела
+        elif 'group' in user_id:
+            department_users = b.get_all('user.get', {'filter': {'UF_DEPARTMENT': user_id[8:]}})
+            for user in department_users:
+                users_id_set.add(user['ID'])
+
+    return list(users_id_set)
+
+
+def get_fio_from_user_info(user_info: dict) -> str:
+    """
+    Возвращает строку, состоящую из фамилии и имени пользователя Б24
+
+    :param user_info: Словарь, полученный запросом с методом user.get
+    """
+
+    return f'{user_info["LAST_NAME"] if "LAST_NAME" in user_info else ""}'\
+           f' {user_info["NAME"] if "NAME" in user_info else ""}'.strip()
+
+
+def change_sheet_style(sheet) -> None:
+
+    # Изменение ширины
+    for column_cells in sheet.columns:
+        length = max(len(str(cell.value) if cell.value else '') for cell in column_cells)
+        sheet.column_dimensions[get_column_letter(column_cells[0].column)].width = length * 1.1
+
+
+def create_employees_report(req):
+    users_id = get_employee_id(req['users'])
+    users_info = b.get_all('user.get', {
+        'ID': users_id
+    })
+
+    report_year = datetime.now().year
+    report_month = datetime.now().month - 1
+    if report_month == 0:
+        report_month = 12
+        report_year -= 1
+
+    month_filter_start = datetime(day=1, month=report_month, year=report_year)
+    month_filter_end = datetime(day=1, month=datetime.now().month, year=datetime.now().year)
+    ddmmyyyy_pattern = '%d.%m.%Y'
+
+
+    workbook = openpyxl.Workbook()
+    report_name = f'Отчет_по_сотрудникам_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.xlsx'
+    month_names = {
+        1: 'Январь', 2: 'Февраль', 3: 'Март', 4: 'Апрель', 5: 'Май', 6: 'Июнь', 7: 'Июль', 8: 'Август', 9: 'Сентябрь',
+        10: 'Октябрь', 11: 'Ноябрь', 12: 'Декабрь'
+    }
+
+    for index, user_info in enumerate(users_info):
+        user_name = get_fio_from_user_info(user_info)
+        if index == 0:
+            worksheet = workbook.active
+            worksheet.title = user_name
+        else:
+            worksheet = workbook.create_sheet(user_name)
+
+        tasks = b.get_all('tasks.task.list', {
+            'filter': {
+                'RESPONSIBLE_ID': user_info['ID'],
+                '>=CREATED_DATE': month_filter_start.strftime(ddmmyyyy_pattern),
+                '<CREATED_DATE': month_filter_end.strftime(ddmmyyyy_pattern),
+            }
+        })
+
+        completed_tasks = list(filter(lambda x: x['status'] == '5', tasks))
+        service_tasks = list(filter(lambda x: x['groupId'] == '71', tasks))
+        completed_service_tasks = list(filter(lambda x: x['status'] == '5', service_tasks))
+        completed_other_tasks = list(filter(lambda x: x['status'] == '5' and x['groupId'] != '71', tasks))
+        non_completed_other_tasks = list(filter(lambda x: x['status'] != '5' and x['groupId'] != '71', tasks))
+
+        worksheet.append([user_name, '', f'{month_names[report_month]} {report_year}'])
+        worksheet.append(['Рабочих дней', f'100 из 100'])
+        worksheet.append([])
+        worksheet.append(['', 'Незакрытых (и созд. в этом мес)', 'Всего создано в месяце'])
+        worksheet.append(['Незакрытые задачи', len(tasks) - len(completed_tasks), len(tasks)])
+        worksheet.append(['СВ', len(service_tasks) - len(completed_service_tasks), len(service_tasks)])
+        worksheet.append(['Остальные', len(non_completed_other_tasks), len(completed_other_tasks) + len(non_completed_other_tasks)])
+
+        change_sheet_style(worksheet)
+
+    workbook.save(report_name)
+
+    # Загрузка отчета в Битрикс
+    bitrix_folder_id = '600147'
+    with open(report_name, 'rb') as file:
+        report_file = file.read()
+    report_file_base64 = str(base64.b64encode(report_file))[2:]
+    upload_report = b.call('disk.folder.uploadfile', {
+        'id': bitrix_folder_id,
+        'data': {'NAME': report_name},
+        'fileContent': report_file_base64
+    })
+    b.call('im.notify.system.add', {
+        'USER_ID': req['user_id'][5:],
+        'MESSAGE': f'Отчет по активностям сформирован. {upload_report["DETAIL_URL"]}'})
+    os.remove(report_name)
+
+
+'''
+create_employees_report({
+    'users': 'user_311',
+})
+'''

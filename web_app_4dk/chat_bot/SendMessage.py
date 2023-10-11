1f7c448c4e5f266c947abb748331162689166f5e
Max <m.k4rp0v@yandex.ru>
2023-10-11 17:30:16 +0300

    1

--- web_app_4dk/chat_bot/SendMessage.py
+++ web_app_4dk/chat_bot/SendMessage.py
@@ -12,11 +12,13 @@ def bot_send_message(req: dict) -> None:
     :param req: {
     dialog_id: числовое значение id пользователя или id чата в формате <chat13>,
     message: текст сообщения,
+    pass_replace: не заменять нижние подчеркивания переносом строки
     }
     :return:
     """
     dialog_id = req['dialog_id'][5:] if 'user' in req['dialog_id'] else req['dialog_id']
-    message_text = req['message'].replace('_', '\n')
+    if 'pass_replace' not in req or not req['pass_replace']:
+        message_text = req['message'].replace('_', '\n')
     message_list_type = '2657' if 'message_list_type' not in req else req['message_list_type']
     data = {
         'BOT_ID': '495',

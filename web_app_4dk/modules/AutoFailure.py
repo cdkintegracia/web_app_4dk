ad062e2f4cd6637da2b5f9ee5f32b89d1046b756
Max <m.k4rp0v@yandex.ru>
2023-10-17 13:48:07 +0300

    Update AutoFailure.py

--- web_app_4dk/modules/AutoFailure.py
+++ web_app_4dk/modules/AutoFailure.py
@@ -19,7 +19,7 @@ def auto_failure(req):
         if filter_date_month == 13:
             filter_date_month = 1
             filter_date_year += 1
-    filter_date_end = datetime(day=1, month=filter_date_month, year=filter_date.year) - timedelta(days=2)
+    filter_date_end = datetime(day=1, month=filter_date_month, year=filter_date_year) - timedelta(days=2)
     filter_date_end = datetime.strftime(filter_date_end, '%Y-%m-%d')
     filter_date_start = datetime.strftime(filter_date, '%Y-%m-%d')
     deal_types = [

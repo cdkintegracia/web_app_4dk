e484fcecac3759d6acb4687c834fa8226d2e74ba
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-03-12 11:12:29 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -186,9 +186,10 @@ def create_employees_report(req):
 
         last_month_deals_data = read_deals_data_file(report_month, report_year)
         before_last_month_deals_data = read_deals_data_file(before_last_month, before_last_month_year)
-        start_year_deals_data = read_deals_data_file(12, report_year-1)
-        date_check = get_quarter_filter(report_month)    
-        quarter_deals_data = read_deals_data_file(date_check['start_date'].month, date_check['start_date'].year)
+        start_year_deals_data = read_deals_data_file(12, datetime.now().year-1)
+
+        date_quarter = get_quarter_filter(report_month)['start_date'] - timedelta(days=1)
+        quarter_deals_data = read_deals_data_file(date_quarter.month, date_quarter.year)
         before_before_last_month_deals_data = read_deals_data_file(before_before_last_month, before_before_last_month_year)
 
         its_deals_last_month = list(filter(lambda x: x['Ответственный'] == user_name and

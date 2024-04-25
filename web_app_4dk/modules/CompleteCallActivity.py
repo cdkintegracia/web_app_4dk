44a82e4fb015f1af82b9079da8d595a1910e999b
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-04-25 09:50:10 +0300

    Update CompleteCallActivity.py

--- web_app_4dk/modules/CompleteCallActivity.py
+++ web_app_4dk/modules/CompleteCallActivity.py
@@ -1,13 +1,10 @@
 import requests
+import datetime
 
 from web_app_4dk.modules.authentication import authentication
 
-
 def complete_call_activity(req):
-<<<<<<< Updated upstream
-=======
     time_change = datetime.datetime.now()
->>>>>>> Stashed changes
     activity_id = req['data[FIELDS][ID]']
     req_data = {
         'filter': {
@@ -15,16 +12,18 @@ def complete_call_activity(req):
             'COMPLETED': 'N',
             'ID': activity_id
         }}
+    req_data_2 = {
+        'filter': {
+            'OWNER_TYPE_ID': '150', #id смарт-процесса
+            'ID': activity_id #id дела
+        }}
     activity_info = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", json=req_data).json()['result']
     if activity_info:
         req_data = {'id': activity_id, 'fields': {'COMPLETED': 'Y'}}
         requests.post(url=f"{authentication('Bitrix')}crm.activity.update", json=req_data)
-<<<<<<< Updated upstream
-=======
     else:
         activity_info_2 = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", json=req_data_2).json()['result']
         if activity_info_2:
             id_element = activity_info_2[0]['OWNER_ID'] #id элемента смарт-процесса
             req_data = {'entityTypeId': 150, 'id': id_element, 'fields': {'ufCrm49ServiceField': time_change}}
             requests.post(url=f"{authentication('Bitrix')}crm.item.update", json=req_data_2)
->>>>>>> Stashed changes

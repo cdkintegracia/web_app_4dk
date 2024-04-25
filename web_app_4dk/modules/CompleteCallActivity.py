885c3e1962064c4f1995a8dbf7b09cd097cc9f41
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-04-25 09:49:03 +0300

    Update CompleteCallActivity.py

--- web_app_4dk/modules/CompleteCallActivity.py
+++ web_app_4dk/modules/CompleteCallActivity.py
@@ -4,6 +4,10 @@ from web_app_4dk.modules.authentication import authentication
 
 
 def complete_call_activity(req):
+<<<<<<< Updated upstream
+=======
+    time_change = datetime.datetime.now()
+>>>>>>> Stashed changes
     activity_id = req['data[FIELDS][ID]']
     req_data = {
         'filter': {
@@ -15,3 +19,12 @@ def complete_call_activity(req):
     if activity_info:
         req_data = {'id': activity_id, 'fields': {'COMPLETED': 'Y'}}
         requests.post(url=f"{authentication('Bitrix')}crm.activity.update", json=req_data)
+<<<<<<< Updated upstream
+=======
+    else:
+        activity_info_2 = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", json=req_data_2).json()['result']
+        if activity_info_2:
+            id_element = activity_info_2[0]['OWNER_ID'] #id элемента смарт-процесса
+            req_data = {'entityTypeId': 150, 'id': id_element, 'fields': {'ufCrm49ServiceField': time_change}}
+            requests.post(url=f"{authentication('Bitrix')}crm.item.update", json=req_data_2)
+>>>>>>> Stashed changes

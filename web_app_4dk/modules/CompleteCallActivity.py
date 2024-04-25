72e3885465e491b7d34de403c559f42e8cad2fee
cdkintegracia <148532247+cdkintegracia@users.noreply.github.com>
2024-04-25 21:01:04 +0300

    Update CompleteCallActivity.py

--- web_app_4dk/modules/CompleteCallActivity.py
+++ web_app_4dk/modules/CompleteCallActivity.py
@@ -4,7 +4,7 @@ import datetime
 from web_app_4dk.modules.authentication import authentication
 
 def complete_call_activity(req):
-    #time_change = datetime.datetime.now()
+    print(req)
     time = datetime.datetime.now()
     time_change = time.strftime('%Y-%m-%d %H:%M')
     activity_id = req['data[FIELDS][ID]']
@@ -21,15 +21,11 @@ def complete_call_activity(req):
         }}
     activity_info = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", json=req_data).json()['result']
     if activity_info:
-        print('0')
         req_data = {'id': activity_id, 'fields': {'COMPLETED': 'Y'}}
         requests.post(url=f"{authentication('Bitrix')}crm.activity.update", json=req_data)
     else:
         activity_info_2 = requests.post(url=f"{authentication('Bitrix')}crm.activity.list", json=req_data_2).json()['result']
-        print('1')
         if activity_info_2:
-            print('2')
             id_element = activity_info_2[0]['OWNER_ID'] #id элемента смарт-процесса
             req_data2 = {'entityTypeId': 150, 'id': id_element, 'fields': {'ufCrm49_ServiceField': time_change}}
-            print(req_data2)
             requests.post(url=f"{authentication('Bitrix')}crm.item.update", json=req_data2)

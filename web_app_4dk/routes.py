731e3517cba446e9c9b3cd578a9906ab6ecf259e
wtfetc <ib09@list.ru>
2024-07-09 11:50:31 +0300

    2

--- web_app_4dk/routes.py
+++ web_app_4dk/routes.py
@@ -86,6 +86,8 @@ from web_app_4dk.modules.AutoFailureLog import auto_failure_log
 #from web_app_4dk.modules.ActivityUpdate import activity_update
 #2024-06-21
 from web_app_4dk.modules.CreateConsultLineTaskReport import create_consultline_task_report
+#2024-07-09
+from web_app_4dk.modules.CreateCompanyFromINN import create_company_from_inn
 
 
 # Словарь функций для вызова из кастомного запроса
@@ -145,7 +147,9 @@ custom_webhooks = {
     #2024-02-27
     'add_auditors_and_send_message': add_auditors_and_send_message,
     'auto_failure_log': auto_failure_log,
-    'create_consultline_task_report': create_consultline_task_report
+    'create_consultline_task_report': create_consultline_task_report,
+    #2024-07-09
+    'create_company_from_inn': create_company_from_inn
 }
 
 # Словарь функций для вызова из запроса со стандартным методом

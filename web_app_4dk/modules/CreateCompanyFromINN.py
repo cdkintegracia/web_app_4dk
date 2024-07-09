30748717c99725053617883d1cb6edb362a8cf0a
wtfetc <ib09@list.ru>
2024-07-09 12:17:07 +0300

    Update CreateCompanyFromINN.py

--- web_app_4dk/modules/CreateCompanyFromINN.py
+++ web_app_4dk/modules/CreateCompanyFromINN.py
@@ -103,7 +103,7 @@ def create_company_from_inn(req):
                         'UF_CRM_1712668987 ': new_company_id,
                         'STAGE_ID': 'UC_0XG0S5'
                         }
-                    }')
+                    })
 
 if __name__ == '__main__':
     create_company_from_inn()

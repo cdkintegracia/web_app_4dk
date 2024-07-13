d699d77742e64c4573a59b0fe869aa8cb93c503e
wtfetc <ib09@list.ru>
2024-07-13 11:34:11 +0300

    Update CreateServiceSalesReport.py

--- web_app_4dk/modules/CreateServiceSalesReport.py
+++ web_app_4dk/modules/CreateServiceSalesReport.py
@@ -161,10 +161,14 @@ def deal_info_handler(deals_info, users_info, month, edo_list_elements=None):
             deal_start_date = get_service_deal_start_dates(month, deal_info['Тип'], deal_info['Предполагаемая дата закрытия'], deal_info['Дата начала'], deal_info['ID'])
 
             if deal_info['Тип'] in service_deal_current_month:
+                if deal_info['Тип'] == 'Доки':
+                    print (deal_info['ID']), deal_start_date)
                 if deal_start_date in deal_info['Дата начала'].strftime('%d.%m.%Y'):
                     #if deal_info['ID'] == '81433':
                         #print(deal_start_date, deal_info, deal_value)
                     deal_value = get_deal_value(deal_info['Сумма'], deal_info['Тип'], deal_info['ID'])
+                    if deal_info['Тип'] == 'Доки':
+                        print (deal_info['ID']))
                     handled_data[deal_info['Ответственный']][service_deal_value_field] += deal_value
                     handled_data[deal_info['Ответственный']]['Сервисы'][f"{month} {deal_info['Тип']}"] += deal_value
 

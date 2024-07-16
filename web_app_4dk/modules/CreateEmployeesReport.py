24c67126b4a96d80d4c97fc29beebd0cb9c3db7a
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-07-16 15:37:27 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -1437,7 +1437,7 @@ def create_employees_report(req):
             #list_of_sales.extend([{'TYPE': 'Тип сделки', 'COMPANY': 'Компания', 'OPPORTUNITY': 'Сумма'}])
             for deal_last_month in sold_deals:
                 deal = list(filter(lambda x: x['ID'] == deal_last_month['ID'], last_month_deals_data))
-                company_title = set(map(lambda x: x['Компания'], list(filter(lambda x: x['ID'] == deal_last_month['COMPANY_ID'], last_month_deals_data))))
+                company_title = list(filter(lambda x: x['ID'] == deal_last_month['COMPANY_ID'], last_month_deals_data))['Компания']
                 if deal:
                     #list_of_sales.append([{'TYPE': deal['Тип'], 'COMPANY': deal['Компания'], 'OPPORTUNITY': deal['Сумма']}])
                     list_of_sales.append({'TYPE': deal[0]['Тип'], 'COMPANY': company_title, 'OPPORTUNITY': deal[0]['Сумма']})

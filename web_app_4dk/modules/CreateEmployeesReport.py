472d53577bead2d9cd464056c82516c394218e3e
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-07-19 17:45:46 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -1405,7 +1405,7 @@ def create_employees_report(req):
             #поиск источников без сделок
             sourse_sans_deals = list(filter(lambda x: not x['parentId2'], sales))
             if sourse_sans_deals:
-                fields_sales = b.get_all('crm.item.fields', {'entityTypeId': '133'})[0]
+                fields_sales = b.get_all('crm.item.fields', {'entityTypeId': '133'})["result"]
                 field_type_source = fields_sales[0]
                 print(field_type_source)
                 sales_not_deals = ([{'TYPE': 'Тип источника', 'COMPANY': 'Компания', 'OPPORTUNITY': 'Сумма'}])

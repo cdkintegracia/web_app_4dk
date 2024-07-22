5dc9ce3ef966947b722c2022552b374720424795
wtfetc <148685666+wtfetc@users.noreply.github.com>
2024-07-22 09:48:55 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -1425,13 +1425,15 @@ def create_employees_report(req):
                     list_of_sales.append({'TYPE': deal['Тип'], 'COMPANY': title[0], 'OPPORTUNITY': deal['Сумма']})
             #массив с инфой о продажах без сделок
             for source_last_month in sourse_sans_deals:
-                title_source = list(set(map(lambda x: x["VALUE"], list(filter(lambda x: x['ID'] == source_last_month['ufCrm3_1654248332'], field_type_source)))))
+                ts = list(filter(lambda x: x['ID'] == source_last_month['ufCrm3_1654248332'], field_type_source))
+                print(ts)
+                title_source = list(set(map(lambda x: x['VALUE'], ts)))
                 title_company = list(set(map(lambda x: x['TITLE'], list(filter(lambda x: x['ID'] == source_last_month['companyId'], company_titles)))))
                 print(title_source)
                 print(title_company)
-                print(source_last_month)
+                #print(source_last_month)
                 if source_last_month:
-                    sales_not_deals.append({'TYPE': title_source, 'COMPANY': title_company[0], 'OPPORTUNITY': source_last_month['opportunity']})
+                    sales_not_deals.append({'TYPE': title_source[0], 'COMPANY': title_company[0], 'OPPORTUNITY': source_last_month['opportunity']})
         else:
             sold_deals = []
 

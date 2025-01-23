b25b805edbdc5836516c75bc7d0dd2d9b7281c7f
wtfetc <ib09@list.ru>
2025-01-23 12:48:15 +0300

    обратная связь

--- web_app_4dk/modules/SendSatisfactionAssessmentMessage_test.py
+++ web_app_4dk/modules/SendSatisfactionAssessmentMessage_test.py
@@ -1,17 +1,21 @@
 from fast_bitrix24 import Bitrix
+import base64
+import logging
 
-from web_app_4dk.modules.authentication import authentication
-from web_app_4dk.modules.ClearContactPhoneNumbers import clear_contact_phone_numbers
+#from web_app_4dk.modules.authentication import authentication
+#from web_app_4dk.modules.ClearContactPhoneNumbers import clear_contact_phone_numbers
+#logging.getLogger('fast_bitrix24').addHandler(logging.StreamHandler())
+
+qwe = base64.b64decode('aHR0cHM6Ly92YzRkay5iaXRyaXgyNC5ydS9yZXN0LzEvaG51dG8yanZvazY2cHVwbS8=').decode('utf-8')
+b = Bitrix(qwe)
 
 
-b = Bitrix(authentication('Bitrix'))
+def send_satisfaction_assessment_message(r):
+    #test = b.get_all('tasks.task.get', {'taskId': r, 'select': ['*', 'UF_*']})
+    #print(test)
 
-
-def send_satisfaction_assessment_message(req):
-    test = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})
-    print(test)
-    #task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})['task']
-    task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})
+    task_info = b.get_all('tasks.task.get', {'taskId': r, 'select': ['*', 'UF_*']})
+    print(task_info)
     if not task_info:
         return
     contact_id = list(filter(lambda x: 'C_' in x, task_info['ufCrmTask']))
@@ -51,16 +55,17 @@ def send_satisfaction_assessment_message(req):
         'entityTypeId': '160',
         'fields': {
             'ufCrm39_1687268735735': call_phone_number,
-            'ufCrm39_1687176023': req['task_id'],
+            'ufCrm39_1687176023': r,
             'ufCrm39_1687955868': contact_email,
-            'title': req['task_id'],
+            'title': r,
             'assigned_by_id': '173',
             'created_by': '173',
         }
     })
 
-    clear_contact_phone_numbers(contact_id)
+#    clear_contact_phone_numbers(contact_id)
 
+    
     '''
     b.call('bizproc.workflow.start', {
         'TEMPLATE_ID': '1561',
@@ -70,3 +75,6 @@ def send_satisfaction_assessment_message(req):
                     }
     })
     '''
+
+if __name__ == '__main__':
+    send_satisfaction_assessment_message(380621)

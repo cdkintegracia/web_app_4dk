0c1f04c755ad0732414ea82004b40e3a569ce124
wtfetc <148685666+wtfetc@users.noreply.github.com>
2025-02-19 13:29:44 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -1477,9 +1477,9 @@ def create_employees_report(req):
         })
         print(user_info['ID'])
         print(len(single_service))
-        print(single_service)
+        print(single_service[0]['ufCrm41_Provider'])
 
-        provide_services = list(filter(lambda x: {x['ufCrm41_Provider'] == user_info['ID']}, single_service))
+        provide_services = list(filter(lambda x: {x['ufCrm41_Provider']} == user_info['ID'], single_service))
         print(len(provide_services))
         sum_provide_services = sum(list(map(lambda x: float(x['ufCrm41_1689101328'] if x['ufCrm41_1689101328'] else 0.0), provide_services)))
 

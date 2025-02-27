b019fcd60bb8619e728134d65af6d779757bc755
wtfetc <148685666+wtfetc@users.noreply.github.com>
2025-02-27 16:18:06 +0300

    Update SendSatisfactionAssessmentMessage.py

--- web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
+++ web_app_4dk/modules/SendSatisfactionAssessmentMessage.py
@@ -10,8 +10,8 @@ b = Bitrix(authentication('Bitrix'))
 def send_satisfaction_assessment_message(req):
     #test = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})
     #print(test)
-    #task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})['task']
-    task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})
+    task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})['task']
+    #task_info = b.get_all('tasks.task.get', {'taskId': req['task_id'], 'select': ['*', 'UF_*']})
     if not task_info:
         return
     contact_id = list(filter(lambda x: 'C_' in x, task_info['ufCrmTask']))

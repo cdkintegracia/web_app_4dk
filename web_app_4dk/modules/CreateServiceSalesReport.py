3c00a03cb9a018c5502be4735832750db0eafb7b
wtfetc <ib09@list.ru>
2025-05-28 21:29:19 +0300

    Update CreateServiceSalesReport.py

--- web_app_4dk/modules/CreateServiceSalesReport.py
+++ web_app_4dk/modules/CreateServiceSalesReport.py
@@ -261,8 +261,8 @@ def write_data_to_xlsx(data, month_titles=None, service_titles=None, month_count
     services_months_summary = dict(zip(month_names, [0 for _ in month_names]))
     for department in departments:
         for employee in data:
-            #print (data[employee])
-            #print(department)
+            print (data[employee])
+            print(department)
             row = []
             its_value = 0
             service_value = 0

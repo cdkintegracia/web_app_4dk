184948a1607ab6768041505a7d252df6221c3979
wtfetc <148685666+wtfetc@users.noreply.github.com>
2025-09-01 17:39:00 +0300

    Update test_CreateCallRedirectionTasks.py

--- web_app_4dk/modules/test_CreateCallRedirectionTasks.py
+++ web_app_4dk/modules/test_CreateCallRedirectionTasks.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 from fast_bitrix24 import Bitrix
 
-from authentication import authentication
+from web_app_4dk.modules.authentication import authentication
 
 
 b = Bitrix(authentication('Bitrix'))

086ef5c495d4b95fb81d4516e4264a10ebb54e3c
wtfetc <148685666+wtfetc@users.noreply.github.com>
2025-09-02 11:54:26 +0300

    Update test_CreateCallRedirectionTasks.py

--- web_app_4dk/modules/test_CreateCallRedirectionTasks.py
+++ web_app_4dk/modules/test_CreateCallRedirectionTasks.py
@@ -23,24 +23,26 @@ def create_call_redirection_tasks(req):
         user_id = list(element['PROPERTY_1235'].values())[0]
         user_info = b.get_all('user.get', {'id': user_id})[0]
         user_name = f"{user_info['LAST_NAME']} {user_info['NAME']}"
-        print(user_info)
         work_phone = f"{user_info['WORK_PHONE']}"
-        print(user_info['UF_DEPARTMENT'][0])
-        department_info = b.get_all('department.get', {'ID': user_info['UF_DEPARTMENT'][0]})[0]
+        if user_info['UF_DEPARTMENT'][0] == 363:
+            #depart_head = '159'
+            depart_head = '1391'
+        else:
+            department_info = b.get_all('department.get', {'ID': user_info['UF_DEPARTMENT'][0]})[0]
+            depart_head = department_info['UF_HEAD']
         vacation_start = list(element['PROPERTY_1233'].values())[0]
         vacation_end = list(element['PROPERTY_1237'].values())[0]
-        '''
+        
         b.call('tasks.task.add', {
             'fields': {
                 'TITLE': f"Переадресация с {user_name} {vacation_start} - {vacation_end}",
                 'DESCRIPTION': f"{user_name} ({work_phone}) уходит в отпуск с {vacation_start} по {vacation_end}. В связи с этим необходимо настроить переадресацию звонков с этого сотрудника на другого",
                 'CREATED_BY': '173',
-                'RESPONSIBLE_ID': department_info['UF_HEAD'],
+                'RESPONSIBLE_ID': depart_head,
                 #'ACCOMPLICES': ['133', ],
                 'DEADLINE': datetime.now().strftime('%Y-%m-%d 19:00:00'),
             }
         })
-        '''
 
 if __name__ == '__main__':
     create_call_redirection_tasks()

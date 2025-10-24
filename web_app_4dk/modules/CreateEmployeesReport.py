fa172d58145abb38b86ed434a346f6dec621431e
wtfetc <148685666+wtfetc@users.noreply.github.com>
2025-10-24 12:43:50 +0300

    Update CreateEmployeesReport.py

--- web_app_4dk/modules/CreateEmployeesReport.py
+++ web_app_4dk/modules/CreateEmployeesReport.py
@@ -1445,13 +1445,10 @@ def create_employees_report(req):
                                     'MESSAGE': f'Проблемы при поиске сделки в файле по источнику продаж\n\n{deal_last_month}'})
                     '''
                     for sale in sales:
-                        print(sale['parentId2'])
                         try:
-                            title_deal = list(filter(lambda x: int(x['ID']) == sale['parentId2'], last_month_deals_data))
-                            print(last_month_deals_data[0])
-                            title_deal = title_deal[0]['Название сделки']
+                            title_deal = list(filter(lambda x: int(x['ID']) == sale['parentId2'], last_month_deals_data))[0]['Название сделки']
                             print(title_deal)
-                            title_company = list(set(map(lambda x: x['TITLE'], list(filter(lambda x: x['ID'] == sale['companyId'], company_titles)))))[0]
+                            title_company = list(set(map(lambda x: x['TITLE'], list(filter(lambda x: int(x['ID']) == sale['companyId'], company_titles)))))[0]
                             if deal:
                                 list_of_sales.append({'NAME_DEAL': title_deal, 'COMPANY': title_company, 'OPPORTUNITY': sale['opportunity']})
                         except:

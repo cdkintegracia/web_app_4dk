d5d361a700da9029ae3c464a2bfc2df89c8f7e25
wtfetc <148685666+wtfetc@users.noreply.github.com>
2026-02-09 10:07:51 +0300

    Update routes.py

--- web_app_4dk/routes.py
+++ web_app_4dk/routes.py
@@ -113,7 +113,7 @@ from web_app_4dk.modules.CountRAReport import count_ra_report
 #2025-12-10 saa
 from web_app_4dk.modules.CA_DownOverTimeReport import ca_downovertime_report
 #2026-01-23 saa
-from web_app_4dk.modules.ClearTitleCompany import clear_title_company
+#from web_app_4dk.modules.ClearTitleCompany import clear_title_company
 
 
 # Словарь функций для вызова из кастомного запроса
@@ -199,7 +199,7 @@ custom_webhooks = {
     #2025-12-10 saa
     'ca_downovertime_report': ca_downovertime_report,
     #2026-01-23 saa
-    'clear_title_company': clear_title_company,
+    #'clear_title_company': clear_title_company,
 }
 
 # Словарь функций для вызова из запроса со стандартным методом

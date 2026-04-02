bd36abd420ef6a4f0734aaff35331bbbbf22f6c6
wtfetc <148685666+wtfetc@users.noreply.github.com>
2026-04-02 16:45:52 +0300

    1

---
+++ web_app_4dk/modules/task_test_vip.py
@@ -0,0 +1,19 @@
+from fast_bitrix24 import Bitrix
+from web_app_4dk.modules.authentication import authentication
+
+
+webhook = authentication('Bitrix')
+b = Bitrix(webhook)
+
+def task_test_vip(req):
+    task_id = req['task_id']
+    task_info = b.get_all('tasks.task.get', { # читаем инфо о задаче
+        'taskId': task_id,
+        'select': ['*', 'UF_*', 'TAGS']
+    })
+
+    try:
+        print(task_info['tags'])
+    except:
+        print(task_info)
+        print(2)

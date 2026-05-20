1d88d0a2b9d3e5d3bc5a5ed04c4e0de979fd027d
wtfetc <148685666+wtfetc@users.noreply.github.com>
2026-05-20 14:20:11 +0300

    Update CreatePaidTasksReport.py

--- web_app_4dk/modules/CreatePaidTasksReport.py
+++ web_app_4dk/modules/CreatePaidTasksReport.py
@@ -73,7 +73,7 @@ def report_paid_tasks(req):
     date_to = req['date_to']
 
     extended_date_from = (datetime.strptime(date_from, '%d.%m.%Y') - timedelta(days=31)).strftime('%Y-%m-%d')
-    extended_date_to = (datetime.strptime(date_from, '%d.%m.%Y') + timedelta(days=31)).strftime('%Y-%m-%d')
+    extended_date_to = (datetime.strptime(date_to, '%d.%m.%Y') + timedelta(days=31)).strftime('%Y-%m-%d')
 
     wb = openpyxl.Workbook()
     ws = wb.active
